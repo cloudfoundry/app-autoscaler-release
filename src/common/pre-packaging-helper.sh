362ce83b24ef4cece0cf93c2816c3a8d6256fba7
Tanmoy Pal <tanmoy.pal@sap.com>
2017-01-12 05:44:28 +0000

    Added service-broker in app-autoscaler bosh release

--- src/common/pre-packaging-helper.sh
+++ src/common/pre-packaging-helper.sh
@@ -34,6 +34,27 @@ setup_maven(){
   export PATH=$M2_HOME/bin:$PATH
 }
 
+
+#NPM setup
+setup_npm(){
+  cd ${BUILD_DIR}
+  # Make sure we can see uname
+  export PATH=$PATH:/bin:/usr/bin
+  #unpack NodeJs - we support Mac OS 64bit and Linux 64bit
+  mkdir npm
+  case "$OSTYPE" in
+    darwin*)
+      tar xvf nodejs/node-v6.9.2-darwin-x64.tar.gz -C npm --strip-components=1
+      ;;
+    linux*)
+      tar xvf nodejs/node-v6.9.2-linux-x64.tar.xz -C npm --strip-components=1
+      ;;
+  esac
+  #setup npm path
+  export PATH=${BUILD_DIR}/npm/bin:$PATH
+}
+
+
 #Cleanup Java files from BUILD_DIR
 cleanup_java(){
   cd ${BUILD_DIR}
@@ -47,3 +68,9 @@ cleanup_maven(){
   rm -rf maven
 }
 
+#NPM Cleanup
+cleanup_npm(){
+  cd ${BUILD_DIR}
+  rm -rf npm
+  rm -rf nodejs
+}
