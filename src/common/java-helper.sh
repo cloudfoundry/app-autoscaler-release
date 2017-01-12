a0f74ec505fb2235846a7a45ace3f08d53c1ed99
Tanmoy Pal <tanmoy.pal@sap.com>
2017-01-12 15:49:38 +0000

    pre-packaging-helper file split into Java and Node helper

--- src/common/java-helper.sh
+++ src/common/java-helper.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #Java Setup
 setup_java(){
   cd ${BUILD_DIR}
@@ -34,27 +36,6 @@ setup_maven(){
   export PATH=$M2_HOME/bin:$PATH
 }
 
-
-#NPM setup
-setup_npm(){
-  cd ${BUILD_DIR}
-  # Make sure we can see uname
-  export PATH=$PATH:/bin:/usr/bin
-  #unpack NodeJs - we support Mac OS 64bit and Linux 64bit
-  mkdir npm
-  case "$OSTYPE" in
-    darwin*)
-      tar xvf nodejs/node-v6.9.2-darwin-x64.tar.gz -C npm --strip-components=1
-      ;;
-    linux*)
-      tar xvf nodejs/node-v6.9.2-linux-x64.tar.xz -C npm --strip-components=1
-      ;;
-  esac
-  #setup npm path
-  export PATH=${BUILD_DIR}/npm/bin:$PATH
-}
-
-
 #Cleanup Java files from BUILD_DIR
 cleanup_java(){
   cd ${BUILD_DIR}
@@ -68,9 +49,3 @@ cleanup_maven(){
   rm -rf maven
 }
 
-#NPM Cleanup
-cleanup_npm(){
-  cd ${BUILD_DIR}
-  rm -rf npm
-  rm -rf nodejs
-}
---
+++ src/common/node-helper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#NPM setup
+setup_npm(){
+  cd ${BUILD_DIR}
+  # Make sure we can see uname
+  export PATH=$PATH:/bin:/usr/bin
+  #unpack NodeJs - we support Mac OS 64bit and Linux 64bit
+  mkdir npm
+  case "$OSTYPE" in
+    darwin*)
+      tar xvf nodejs/node-v6.9.2-darwin-x64.tar.gz -C npm --strip-components=1
+      ;;
+    linux*)
+      tar xvf nodejs/node-v6.9.2-linux-x64.tar.xz -C npm --strip-components=1
+      ;;
+  esac
+  #setup npm path
+  export PATH=${BUILD_DIR}/npm/bin:$PATH
+}
+
+#NPM Cleanup
+cleanup_npm(){
+  cd ${BUILD_DIR}
+  rm -rf npm
+  rm -rf nodejs
+}
+
