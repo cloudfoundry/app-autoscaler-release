6c9d79d11c552ebec03d16993b34775e2d5b100a
Rohit Sharma <rohit.sharma04@sap.com>
2017-05-15 15:37:56 +0530

    Enable org level service access in acceptance tests

--- src/acceptance/broker/broker_suite_test.go
+++ src/acceptance/broker/broker_suite_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"acceptance/config"
+	. "acceptance/helpers"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
@@ -39,10 +40,17 @@ var _ = BeforeSuite(func() {
 	setup = workflowhelpers.NewTestSuiteSetup(cfg)
 	setup.Setup()
 
+	workflowhelpers.AsUser(setup.AdminUserContext(), cfg.DefaultTimeoutDuration(), func() {
+		EnableServiceAccess(cfg, setup.GetOrganizationName())
+	})
+
 	serviceExists := cf.Cf("marketplace", "-s", cfg.ServiceName).Wait(cfg.DefaultTimeoutDuration())
 	Expect(serviceExists).To(Exit(0), fmt.Sprintf("Service offering, %s, does not exist", cfg.ServiceName))
 })
 
 var _ = AfterSuite(func() {
+	workflowhelpers.AsUser(setup.AdminUserContext(), cfg.DefaultTimeoutDuration(), func() {
+		DisableServiceAccess(cfg, setup.GetOrganizationName())
+	})
 	setup.Teardown()
 })
