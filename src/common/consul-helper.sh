66f4cc5f2587e3f9ed46d3f1f8b0b48773c05a8a
qiyangbj@cn.ibm.com <qiyangbj@cn.ibm.com>
2018-07-01 10:39:31 +0800

    make wait_consul_agent a function

--- src/common/consul-helper.sh
+++ src/common/consul-helper.sh
@@ -10,3 +10,20 @@ function start_consul_agent {
     set -e
   fi
 }
+
+function wait_consul_agent {
+  HOST=$1
+  PORT=$2
+  retry=0
+  while [[ $retry -lt 10 ]]
+  do
+    nc -z $HOST $PORT
+    if [[ $? -eq 0 ]]
+    then
+      break
+    fi
+    sleep 5
+    let retry=$retry+1
+    echo "wait consul agent starting: $retry"
+  done
+}
