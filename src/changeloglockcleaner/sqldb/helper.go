1ce2e34836e5c6eea019d2a5378206d575e1b6f0
an qin <aqan@cn.ibm.com>
2020-01-09 10:11:34 +0800

    remove unused code and modify changelogdb

--- src/changeloglockcleaner/sqldb/helper.go
+++ src/changeloglockcleaner/sqldb/helper.go
@@ -6,6 +6,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"io/ioutil"
+	"net/url"
 	"github.com/go-sql-driver/mysql"
 
 )
@@ -15,89 +16,74 @@ type Database struct {
 	DSN         string
 }
 
-func Connection(dbUrl string) (*Database, error) {
-	var dsn string
-	var tlsValue string
-	var sslrootcert string
-	database := &Database{}
-	queryString := "?parseTime=true"
+type MySQLConfig struct {
+	config   *mysql.Config
+	cert     string
+}
 
-	database.DriverName = DetectDirver(dbUrl)
+/**
+ This function is used to generate db connection info, for example, 
+ For mysql: 
+ input dbUrl: 'username:password@tcp(localhost:3306)/autoscaler?tls=custom&sslrootcert=db_ca.crt'
+   return:
+   &Database{DriverName: "mysql", DSN:"username:password@tcp(localhost:3306)/autoscaler?parseTime=true&tls=custom"}
+ 
+For postgres:
+   input dbUrl: postgres://postgres:password@localhost:5432/autoscaler?sslmode=disable
+   return:
+   &Database{DriverName: "postgres", DSN:"postgres://postgres:password@localhost:5432/autoscaler?sslmode=disable"
+ **/
+ func GetConnection(dbUrl string) (*Database, error) {
+	database := &Database{}
+
+	database.DriverName = detectDirver(dbUrl)
 
 	switch database.DriverName {
 	case MysqlDriverName:
-		if strings.Contains(dbUrl,"?") {
-			params := []string{}
-			urlStrs := strings.Split(dbUrl,"?")
-			paramString := urlStrs[1]
-			for _, v := range strings.Split(paramString, "&") {
-				param := strings.SplitN(v, "=", 2)
-				if len(param) != 2 {
-					continue
-				}
-				if param[0] == "tls" {
-					tlsValue= param[1]
-				}
-				if param[0]=="sslrootcert" {
-					sslrootcert = param[1]
-					continue
-				}
-				params = append(params,v)
-			}
-
-			queryString = fmt.Sprintf("%s&%s", queryString, strings.Join(params,"&"))
-			dsn = urlStrs[0]+queryString
-			database.DSN = dsn
-
-			err :=registerConfig(tlsValue,sslrootcert,dsn)
-			if err !=nil {
-				return nil, err
-			}
-
-		}else {
-			database.DSN = dbUrl
-		}				
+		cfg, err := parseMySQLURL(dbUrl)
+		if err != nil {
+			return nil, err
+		}
+		
+		err = registerConfig(cfg)
+		if err != nil {
+			return nil, err
+		}
+		database.DSN = cfg.config.FormatDSN()				
 	case PostgresDriverName:
 		database.DSN = dbUrl
 	}
 	return database, nil
 }
 
-func registerConfig(tlsValue string, sslrootcert string, url string) error {
-	if _, isBool := readBool(tlsValue); isBool || strings.ToLower(tlsValue) == "skip-verify" || strings.ToLower(tlsValue) == "preferred" {
+func registerConfig(cfg *MySQLConfig) error {
+	tlsValue := cfg.config.TLSConfig 
+	if _, isBool := readBool(tlsValue); isBool || tlsValue == "" || strings.ToLower(tlsValue) == "skip-verify" || strings.ToLower(tlsValue) == "preferred" {
 		// Do nothing here
 		return nil
-	} else if sslrootcert != "" {
-		certBytes, err := ioutil.ReadFile(sslrootcert)
+	} else if cfg.cert != "" {
+		certBytes, err := ioutil.ReadFile(cfg.cert)
 		if err != nil {
-			fmt.Printf("failed to read sql ca file: %v", err)
 			return err
 		}
 		caCertPool := x509.NewCertPool()
 		if ok := caCertPool.AppendCertsFromPEM(certBytes); !ok {
-			fmt.Printf("failed to parse sql ca: %v", err)
 			return err
 		}
 
-		cfg, err := mysql.ParseDSN(url)
-		if err != nil {
-			fmt.Printf("invalid db url %s with error %v",url, err)
-			return err
+		tlsConfig := tls.Config{}
+		tlsConfig.RootCAs = caCertPool
+		if tlsValue == "verify_identity" {	
+			tlsConfig.ServerName = strings.Split(cfg.config.Addr,":")[0]
 		}
-
-		tlsConfig := &tls.Config{
-			InsecureSkipVerify: false,
-			RootCAs:            caCertPool,
-			ServerName:         strings.Split(cfg.Addr,":")[0],
-		}
-		err = mysql.RegisterTLSConfig(tlsValue, tlsConfig)
+	
+		err = mysql.RegisterTLSConfig(tlsValue, &tlsConfig)
 		if err != nil {
-			fmt.Printf("failed to register tlsconfig: %v",err)
 			return err
 		}
 
 	} else {
-		return fmt.Errorf("sql ca file is not provided when tls is a custom key")
+		return fmt.Errorf("sql ca file is not provided")
 	}
 	return nil
 }
@@ -112,7 +98,7 @@ func readBool(input string) (value bool, valid bool) {
 	return
 }
 
-func DetectDirver(dbUrl string)(driver string) {
+func detectDirver(dbUrl string)(driver string) {
 	if strings.Contains(dbUrl, "postgres"){
 		return PostgresDriverName
 	} else {
@@ -120,4 +106,42 @@ func DetectDirver(dbUrl string)(driver string) {
 	}
 }
 
+// parseMySQLURL can parse the query parameters and remove invalid 'sslrootcert', it return mysql.Config and the cert file.
+func parseMySQLURL(dbUrl string)( *MySQLConfig, error) {
+	var caCert string
+	var tlsValue string
+	if strings.Contains(dbUrl,"?"){ 
+		u, err := url.ParseQuery(strings.Split(dbUrl,"?")[1])
+		if err != nil {
+		  return nil, err
+		}
+		urlParam := url.Values{}
+		for k, v := range u {
+		  if k == "sslrootcert" {
+			caCert = v[0]
+			continue
+		  }
+		  if k == "tls" {
+			tlsValue = v[0]
+			continue
+		  }
+		  urlParam.Add(k, v[0])
+		}
+		dbUrl = fmt.Sprintf("%s?%s",strings.Split(dbUrl,"?")[0], urlParam.Encode())
+	}
 
+	config, err := mysql.ParseDSN(dbUrl)
+	if err != nil {
+		return nil, err
+	}
+	config.ParseTime = true
+	
+	if tlsValue != "" {
+		config.TLSConfig = tlsValue
+	}
+
+	return &MySQLConfig{
+		config:   config,
+		cert:     caCert,
+	}, nil
+}
