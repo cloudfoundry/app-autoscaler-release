403d30bc46c56ed343277ff7374b431d67884def
Gareth Evans <g.evans@sap.com>
2021-07-14 15:46:24 +0100

    Fixed packaging scripts

--- src/common/java-helper.sh
+++ src/common/java-helper.sh
@@ -2,37 +2,29 @@
 
 #Java Setup
 setup_java(){
-  cd ${BUILD_DIR}
-  # Make sure we can see uname
-  export PATH=$PATH:/bin:/usr/bin
-
-  #unpack Java - we support Mac OS 64bit and Linux 64bit otherwise we require JAVA_HOME to point to JDK
   mkdir java
-  tar zxvf openjdk/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz -C java --strip-components=4
-  export JAVA_HOME=${BUILD_DIR}/java
-
-  #setup Java path
+  tar zxf openjdk/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz -C java --strip-components=4
+  export JAVA_HOME=${BOSH_INSTALL_TARGET}/java
   export PATH=$JAVA_HOME/bin:$PATH
 }
 
 #Maven Setup
 setup_maven(){
-  cd ${BUILD_DIR}
-  tar zxvf maven/apache-maven-3.6.3-bin.tar.gz -C maven --strip-components=1
-  export M2_HOME=${BUILD_DIR}/maven
+  mkdir maven-install
+  tar zxf maven/apache-maven-3.6.3-bin.tar.gz -C maven-install --strip-components=1
+  cp -R maven-install/* ${BOSH_INSTALL_TARGET}
+  export M2_HOME=${BOSH_INSTALL_TARGET}/maven
   export PATH=$M2_HOME/bin:$PATH
 }
 
-#Cleanup Java files from BUILD_DIR
+#Cleanup Java files from BOSH_COMPILE_TARGET
 cleanup_java(){
-  cd ${BUILD_DIR}
   rm -rf java
   rm -rf openjdk
 }
 
-#Cleanup Maven files from BUILD_DIR
+#Cleanup Maven files from BOSH_COMPILE_TARGET
 cleanup_maven(){
-  cd ${BUILD_DIR}
   rm -rf maven
 }
 
