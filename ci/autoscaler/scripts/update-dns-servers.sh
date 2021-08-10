74d678d58a4c33be9baf89dfb2106c39dac1ef42
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-08-10 11:44:20 +0200

    use bbl instead of terraform

--- ci/autoscaler/scripts/update-dns-servers.sh
+++ ci/autoscaler/scripts/update-dns-servers.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 set -x
 
-pushd autoscaler-env-bbl-state
-  terraform output -state=bbl-state/vars/terraform.tfstate -json | jq -r '.system_domain_dns_servers.value'
+pushd autoscaler-env-bbl-state/bbl-state
+  bbl outputs | yq eval '.system_domain_dns_servers | join(" ")' -
 popd
 
 
