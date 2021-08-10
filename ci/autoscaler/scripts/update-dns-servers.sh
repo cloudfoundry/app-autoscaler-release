b7a79ffd7b8bafa5e0bfe27968049b89f258c438
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-08-10 12:10:14 +0200

    get service key

--- ci/autoscaler/scripts/update-dns-servers.sh
+++ ci/autoscaler/scripts/update-dns-servers.sh
@@ -6,4 +6,17 @@ pushd autoscaler-env-bbl-state/bbl-state
   bbl outputs | yq eval '.system_domain_dns_servers | join(" ")' -
 popd
 
+write_gcp_service_account_key
+
+write_gcp_service_account_key() {
+  set +x
+  if [ -f "${BBL_GCP_SERVICE_ACCOUNT_KEY}" ]; then
+    cp "${BBL_GCP_SERVICE_ACCOUNT_KEY}" /tmp/google_service_account.json
+  else
+    echo "${BBL_GCP_SERVICE_ACCOUNT_KEY}" > /tmp/google_service_account.json
+  fi
+  export BBL_GCP_SERVICE_ACCOUNT_KEY="/tmp/google_service_account.json"
+  set -x
+}
+
 
