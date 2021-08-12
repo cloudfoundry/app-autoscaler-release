3796dece73a989c27c3b82d37d9aafca889f400a
Gareth Evans <g.evans@sap.com>
2021-08-12 09:52:31 +0100

    Do not attempt to delete the service broker if it exists

--- ci/autoscaler/scripts/run-acceptance-tests.sh
+++ ci/autoscaler/scripts/run-acceptance-tests.sh
@@ -12,23 +12,18 @@ cf api https://api.${SYSTEM_DOMAIN} --skip-ssl-validation
 CF_ADMIN_PASSWORD=$(credhub get -n /bosh-autoscaler/cf/cf_admin_password -q)
 cf auth admin $CF_ADMIN_PASSWORD
 
-# cf login -a https://api.bosh-lite.com -u admin -p admin --skip-ssl-validation
-
-# set +e
-# cf delete-service-broker -f autoscaler
-# set -e
-
 set +e
 SERVICE_BROKER_EXISTS=$(cf service-brokers | grep -c autoscalerservicebroker.${SYSTEM_DOMAIN}) 
 set -e
 if [[ $SERVICE_BROKER_EXISTS == 1 ]]; then
-  echo "Service Broker already exists, deleting"
-  cf delete-service-broker -f autoscaler
+  echo "Service Broker already exists, assuming this is ok..."
+  #cf delete-service-broker -f autoscaler
+else
+  echo "Creating service broker..."
+  AUTOSCALER_SERVICE_BROKER_PASSWORD=$(credhub get  -n /bosh-autoscaler/app-autoscaler/autoscaler_service_broker_password -q)
+  cf create-service-broker autoscaler autoscaler_service_broker_user $AUTOSCALER_SERVICE_BROKER_PASSWORD https://autoscalerservicebroker.${SYSTEM_DOMAIN}
 fi
 
-echo "Creating service broker..."
-AUTOSCALER_SERVICE_BROKER_PASSWORD=$(credhub get  -n /bosh-autoscaler/app-autoscaler/autoscaler_service_broker_password -q)
-cf create-service-broker autoscaler autoscaler_service_broker_user $AUTOSCALER_SERVICE_BROKER_PASSWORD https://autoscalerservicebroker.${SYSTEM_DOMAIN}
 #cf enable-service-access autoscaler
 
 export GOPATH=$PWD/app-autoscaler-release
