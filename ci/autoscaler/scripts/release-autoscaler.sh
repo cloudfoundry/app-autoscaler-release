50c59a7c41d5a3aaa9fe36fdc56d933d559602bc
Gareth Evans <g.evans@sap.com>
2021-08-16 13:42:59 +0100

    Added tasks to attempt to perform a release

---
+++ ci/autoscaler/scripts/release-autoscaler.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+pushd app-autoscaler-release
+  # determine what the next release version should be
+  NEXT_VERSION=$(jx-release-version)
+  echo "Next Version should be ${NEXT_VERSION}"
+
+  # generate the private.yml file with the credentials
+  cat > config/private.yml <<EOF
+---
+blobstore:
+  options:
+    credentials_source: static
+    json_key: |
+      $UPLOADER_KEY
+EOF
+
+  # REMOVE ME
+  cat config/private.yml
+  git status
+
+  # create bosh release with the specified version
+  bosh create-release --final --version "$NEXT_VERSION" --tarball=releases/app-autoscaler-v"$NEXT_VERSION".tgz
+popd
+
+# create the GitHub release (from the correct sha & branch)
+
+# upload release notes
+
+# fail whilst this is a work in progress
+exit 1
