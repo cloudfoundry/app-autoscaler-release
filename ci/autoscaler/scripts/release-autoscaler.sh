536193a393f079f52b51af4430c7b155e3772b9c
Gareth Evans <g.evans@sap.com>
2021-08-17 11:07:50 +0100

    Use an output of generated-releases to create the tgz file

--- ci/autoscaler/scripts/release-autoscaler.sh
+++ ci/autoscaler/scripts/release-autoscaler.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
+mkdir -p generated-release
+
 pushd app-autoscaler-release
   # determine what the next release version should be
-  NEXT_VERSION=$(jx-release-version)
-  echo "Next Version should be ${NEXT_VERSION}"
+  VERSION=$(jx-release-version)
+  echo "Next Version should be ${VERSION}"
 
   # generate the private.yml file with the credentials
   cat > config/private.yml <<EOF
@@ -23,6 +25,21 @@ EOF
   # create bosh release with the specified version
   bosh create-release \
     --final \
-    --version "$NEXT_VERSION" \
-    --tarball=../release/app-autoscaler-v"$NEXT_VERSION".tgz
+    --version "$VERSION" \
+    --tarball=app-autoscaler-v$VERSION.tgz
+  
+  RELEASE_TGZ=app-autoscaler-v$VERSION.tgz
+  export SHA1=$(sha1sum $RELEASE_TGZ | head -n1 | awk '{print $1}')
+  echo "SHA1=$SHA1"
+
+  export RELEASE_ROOT=../generated-release
+
+  echo "v${VERSION}" > ${RELEASE_ROOT}/tag
+  echo "v${VERSION}" > ${RELEASE_ROOT}/name
+  mkdir -p ${RELEASE_ROOT}/artifacts
+  mv app-autoscaler-v${VERSION}.tgz ${RELEASE_ROOT}/artifacts
+
+  #mv ${REPO_ROOT}/ci/release_notes.md          ${RELEASE_ROOT}/notes.md
+  #cat >> ${RELEASE_ROOT}/notes.md <<EOF
+  
 popd
