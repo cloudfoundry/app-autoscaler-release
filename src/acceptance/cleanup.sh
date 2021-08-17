6bfe7964ed0977229c918fb920f6181fe1244aa3
Gareth Evans <g.evans@sap.com>
2021-08-17 11:27:30 +0100

    Fixed some shellcheck issues

--- src/acceptance/cleanup.sh
+++ src/acceptance/cleanup.sh
@@ -1,20 +1,23 @@
-#!/bin/sh -x
+#!/bin/bash
 
 set -euo pipefail
+set -x
 
 ORG_PREFIX=ASATS
+SERVICE_PREFIX=autoscaler
 
 ORGS=$(cf orgs | grep -v name | grep ${ORG_PREFIX})
 for ORG in $ORGS; do
 	set +e
-	cf delete-org $ORG -f
-	if [ "$?" != "0" ]; then 
-		cf target -o $ORG
-		SERVICES=$(cf services | grep -v name | grep autoscaler | awk '{print $1}')
+	cf delete-org "$ORG" -f
+	# shellcheck disable=SC2181
+	if [ "$?" != "0" ]; then
+		cf target -o "$ORG"
+		SERVICES=$(cf services | grep -v name | grep ${SERVICE_PREFIX} | awk '{print $1}')
 		for SERVICE in $SERVICES; do
-			cf purge-service-instance $SERVICE -f
+			cf purge-service-instance "$SERVICE" -f
 		done
-		cf delete-org -f $ORG
+		cf delete-org -f "$ORG"
 	fi
 	set -e
 done
