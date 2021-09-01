4faa3a9b65da7a385ac30f21c6edd8ef1d3bf40a
Gareth Evans <g.evans@sap.com>
2021-09-01 15:24:17 +0100

    Remove autoscaler entries from credhub

--- ci/autoscaler/scripts/cleanup-autoscaler.sh
+++ ci/autoscaler/scripts/cleanup-autoscaler.sh
@@ -29,3 +29,31 @@ set +e
 bosh delete-deployment -d app-autoscaler -n
 set -e
 
+set +e
+credhub delete -n /bosh-autoscaler/app-autoscaler/postgres_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/postgres_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/metricsserver_client
+credhub delete -n /bosh-autoscaler/app-autoscaler/metricsserver_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/metricsserver_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/scheduler_client
+credhub delete -n /bosh-autoscaler/app-autoscaler/scheduler_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/scheduler_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_public_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_public_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_client
+credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_client
+credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_public_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_public_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/eventgenerator_client
+credhub delete -n /bosh-autoscaler/app-autoscaler/eventgenerator_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/eventgenerator_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/scalingengine_client
+credhub delete -n /bosh-autoscaler/app-autoscaler/scalingengine_server
+credhub delete -n /bosh-autoscaler/app-autoscaler/scalingengine_ca
+credhub delete -n /bosh-autoscaler/app-autoscaler/autoscaler_service_broker_password
+credhub delete -n /bosh-autoscaler/app-autoscaler/database_password
+set -e
