41e2c778c035a33dddf77457cbdb6beaf62ff94a
Alan Moran <alan.moran@sap.com>
2021-10-05 14:38:45 +0200

    Adds register broker task

---
+++ ci/autoscaler/scripts/register-broker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VAR_DIR=bbl-state/bbl-state/vars
+pushd bbl-state/bbl-state
+  eval "$(bbl print-env)"
+popd
+
+cf api https://api.${SYSTEM_DOMAIN} --skip-ssl-validation
+
+CF_ADMIN_PASSWORD=$(credhub get -n /bosh-autoscaler/cf/cf_admin_password -q)
+cf auth admin $CF_ADMIN_PASSWORD
+
+set +e
+SERVICE_BROKER_EXISTS=$(cf service-brokers | grep -c autoscalerservicebroker.${SYSTEM_DOMAIN})
+set -e
+if [[ $SERVICE_BROKER_EXISTS == 1 ]]; then
+  echo "Service Broker already exists, assuming this is ok..."
+  #cf delete-service-broker -f autoscaler
+else
+  echo "Creating service broker..."
+  AUTOSCALER_SERVICE_BROKER_PASSWORD=$(credhub get  -n /bosh-autoscaler/app-autoscaler/autoscaler_service_broker_password -q)
+  cf create-service-broker autoscaler autoscaler_service_broker_user $AUTOSCALER_SERVICE_BROKER_PASSWORD https://autoscalerservicebroker.${SYSTEM_DOMAIN}
+fi
+
+cf logout
