6bd8e20f4f807104df24d8c53a00295ac61a67e5
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-10-20 14:59:18 +0200

    increase timeout 1st attempt

--- src/acceptance/pre_upgrade/pre_dynamic_policy_test.go
+++ src/acceptance/pre_upgrade/pre_dynamic_policy_test.go
@@ -37,13 +37,13 @@ var _ = Describe("AutoScaler dynamic policy", func() {
 
 				By("should scale out to 2 instances")
 				helpers.AppSetCpuUsage(cfg, appName, 50, 5)
-				helpers.WaitForNInstancesRunning(appGUID, 2, 10*time.Minute)
+				helpers.WaitForNInstancesRunning(appGUID, 2, 15*time.Minute)
 
 				By("should scale in to 1 instance after cpu usage is reduced")
 				for i := 0; i < 2; i++ {
 					helpers.AppEndCpuTest(cfg, appName, i)
 				}
-				helpers.WaitForNInstancesRunning(appGUID, 1, 10*time.Minute)
+				helpers.WaitForNInstancesRunning(appGUID, 1, 15*time.Minute)
 
 			})
 
