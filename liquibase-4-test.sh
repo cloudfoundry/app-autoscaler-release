014fd3b6289c5bb5c76b8496add1363f3d7585d2
Gareth Evans <g.evans@sap.com>
2022-01-04 14:24:01 +0000

    Use the versions plugin to set liquibase version

--- liquibase-4-test.sh
+++ liquibase-4-test.sh
@@ -6,13 +6,18 @@ git restore src/db/pom.xml
 rm -f liquibase.properties
 
 make clean
+
+pushd src/db
+  mvn versions:use-dep-version -Dincludes=org.liquibase:liquibase-core -DdepVersion=3.6.3 -DforceVersion=true
+popd
+
 # set liquibase to 3.6.3
-sed -i 's/3.10.3/3.6.3/' src/db/pom.xml
 make init-db | grep -v '##'
 # set liquibase to 4.x.x
 
 sed -i 's/3.6.3/4.6.2/' src/db/pom.xml
 pushd src/db
+  mvn versions:use-dep-version -Dincludes=org.liquibase:liquibase-core -DdepVersion=4.6.2 -DforceVersion=true
   mvn clean package -DskipTests
 popd
 
