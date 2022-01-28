9f75786cb0440eed7e36652ab5a1c6855b811ae5
Kevin Cross <kevin.cross@sap.com>
2022-01-28 12:17:21 +0000

    FIT fix

--- src/acceptance/broker/broker_test.go
+++ src/acceptance/broker/broker_test.go
@@ -99,7 +99,7 @@ var _ = Describe("AutoScaler Service Broker", func() {
 		service.delete()
 	})
 
-	FIt("should fail to update service instance from acceptance-standard to first", func() {
+	It("should fail to update service instance from acceptance-standard to first", func() {
 		plans := getPlans()
 		if plans.length() < 2 {
 			Skip(fmt.Sprintf("2 plans needed, only one plan available plans:%+v", plans))
