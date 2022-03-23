a29d7e02dc121e5df2305d9e2e6ad7285a906707
Silvestre Zabala <silvestre.zabala@sap.com>
2022-03-23 10:19:18 +0100

    Stop hardcoding PostgreSQL version in tests

--- ci/autoscaler/scripts/run-integration-tests.sh
+++ ci/autoscaler/scripts/run-integration-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-pg_ctlcluster 10 main start
+pg_ctlcluster "$(pg_lsclusters -j | jq -r '.[0].version')" main start
 
 psql postgres://postgres@127.0.0.1:5432 -c 'DROP DATABASE IF EXISTS autoscaler'
 psql postgres://postgres@127.0.0.1:5432 -c 'CREATE DATABASE autoscaler'
