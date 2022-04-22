1615592eb82eea7a9051cdeafcbdb59945d0c735
Matthias Vach <matthias.vach@sap.com>
2022-04-22 14:24:49 +0200

    Execute golang and java update in seperate scripts

--- scripts/vendor_package.sh
+++ scripts/vendor_package.sh
@@ -25,18 +25,3 @@ function vendor-package {
     cp "${package_version_file}" "${script_dir}/../packages/${package}"
   fi
 }
-
-vendor-package golang-release golang-1-linux
-vendor-package java-release openjdk-11
-
-golang_version=$( cat "${script_dir}/../packages/golang-1-linux/version")
-stripped_go_version=$(echo "${golang_version}" | cut -d . -f -2)
-
-sed -i '' "s/go 1.*/go $stripped_go_version/g" "${script_dir}/../src/acceptance/go.mod"
-sed -i '' "s/go 1.*/go $stripped_go_version/g" "${script_dir}/../src/autoscaler/go.mod"
-sed -i '' "s/go 1.*/go $stripped_go_version/g" "${script_dir}/../src/changelog/go.mod"
-sed -i '' "s/go 1.*/go $stripped_go_version/g" "${script_dir}/../src/changeloglockcleaner/go.mod"
-
-sed -i '' "s/golang 1.*/golang $golang_version/g" "${script_dir}/../.tool-versions"
-
-
