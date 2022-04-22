d39088d30fefdd29204fb76b8514bcb7b0af1974
Kevin Cross <kevin.cross@sap.com>
2022-04-22 17:33:00 +0100

    updating task script

--- ci/autoscaler/tasks/update-sdk/update_golang_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_golang_package.sh
@@ -12,9 +12,9 @@ vendor-package golang-release golang-1-linux "${autoscaler_dir}"
 golang_version=$( cat "${autoscaler_dir}/packages/golang-1-linux/version")
 stripped_go_version=$(echo "${golang_version}" | cut -d . -f -2)
 
-sed -i '' "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/acceptance/go.mod"
-sed -i '' "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/autoscaler/go.mod"
-sed -i '' "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/changelog/go.mod"
-sed -i '' "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/changeloglockcleaner/go.mod"
+sed -i "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/acceptance/go.mod"
+sed -i "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/autoscaler/go.mod"
+sed -i "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/changelog/go.mod"
+sed -i "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/changeloglockcleaner/go.mod"
 
-sed -i '' "s/golang 1.*/golang $golang_version/g" "${autoscaler_dir}/.tool-versions"
+sed -i "s/golang 1.*/golang $golang_version/g" "${autoscaler_dir}/.tool-versions"
