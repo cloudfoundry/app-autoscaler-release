ffc0408df3baf501261c18c0184767edbdc8b04d
Kevin Cross <kevin.cross@sap.com>
2022-04-22 17:12:30 +0100

    updating task script

--- ci/autoscaler/tasks/update-sdk/update_golang_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_golang_package.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 source "${script_dir}/vendor_package.sh"
-
-vendor-package golang-release golang-1-linux app-autoscaler-release
-
-golang_version=$( cat "${script_dir}/../packages/golang-1-linux/version")
-stripped_go_version=$(echo "${golang_version}" | cut -d . -f -2)
 autoscaler_dir=app-autoscaler-release
+
+vendor-package golang-release golang-1-linux "${autoscaler_dir}"
+
+golang_version=$( cat "${autoscaler_dir}/packages/golang-1-linux/version")
+stripped_go_version=$(echo "${golang_version}" | cut -d . -f -2)
 
 sed -i '' "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/acceptance/go.mod"
 sed -i '' "s/^[[:space:]]*go 1.*/go $stripped_go_version/g" "${autoscaler_dir}/src/autoscaler/go.mod"
