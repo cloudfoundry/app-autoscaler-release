6396374e495becd3da5c5ca669cbc8e7ba4d52f6
Matthias Vach <matthias.vach@sap.com>
2022-04-22 14:45:42 +0200

    Setup concourse jobs to update sdks

--- ci/autoscaler/scripts/create-pr.sh
+++ ci/autoscaler/scripts/create-pr.sh
@@ -1,48 +1,26 @@
 #!/usr/bin/env bash
 
-set -eu
+set -euo pipefail
 
-: "${GITHUB_TOKEN:?}"
+function golang_version {
+  version=$(<packages/golang-1-linux/version)
+  echo $version | sed s/\\./-/g
+}
+
+function java_version {
+  cat packages/openjdk-11/spec | grep -e "- jdk-" | sed -E 's/- jdk-(.*)\.tar\.gz/\1/g'
+}
 
 pushd app-autoscaler-release-main
-  if git show --pretty="" --name-only | grep -q packages/golang-1-linux/spec.lock; then
-    version=$(<packages/golang-1-linux/version)
-    dashed_version=$(echo $version | sed s/\\./-/g )
+  version=$(${type}_version)
+  dashed_version=$(echo $version | sed s/\\./-/g )
 
-    UPDATE_BRANCH="golang-version-bump-${dashed_version}"
-    PR_TITLE="Update golang version to ${version}"
-    PR_DESCRIPTION="Automatic version bump of golang to ${version}"
+  update_branch="${type}-version-bump-${dashed_version}"
+  pr_title="Update ${type} version to ${version}"
+  pr_description="Automatic version bump of ${type} to ${version}"
 
-  elif git show --pretty="" --name-only | grep -q packages/packages/openjdk-11/spec.lock; then
-    UPDATE_BRANCH="java-version-bump"
-    PR_TITLE="Update java version"
-    PR_DESCRIPTION="Automatic version bump of java"
-
-  else
-    exit 1
-  fi
-
-
-  BODY=("\"head\": \"${UPDATE_BRANCH}\"," "\"base\": \"main\"," "\"title\": \"${PR_TITLE}\"," "\"body\": \"${PR_DESCRIPTION}\"")
-  BODY_JSON="{${BODY[*]}}"
-  API_URL="https://api.github.com/repos/cloudfoundry/app-autoscaler-release/pulls"
-
-  echo "Creating PR on ${UPDATE_BRANCH} ..."
-  RESPONSE=$(curl -i -s -o - -X POST -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -d "${BODY_JSON}" ${API_URL})
-  RESPONSE_CODE=$(echo "${RESPONSE}" | grep HTTP | awk '{print $2}')
-
-  if [ "${RESPONSE_CODE}" == "422" ]; then
-    EXISTING_PR="$( curl -s -o - -H "Authorization: token ${GITHUB_TOKEN}" "${API_URL}?head=cloudfoundry:${UPDATE_BRANCH}" )"
-    URL=$(echo "${EXISTING_PR}" | jq .[0].html_url)
-    echo "PR has already been created: ${URL}"
-    exit 0
-  else
-    if [ "${RESPONSE_CODE}" != "201" ]; then
-      echo "Error while creating PR. HTTP return code was ${RESPONSE_CODE}. Exiting."
-      exit 1
-    fi
-  fi
+  git checkout -b ${update_branch}
+  git commit -a -m "${pr_title}"
+  gh auth login --with-token "${github_token}"
+  gh pr create --base origin/main --title "${pr_title}" --body "${pr_description}"
 popd
-
-URL=$(echo "${RESPONSE}" | grep -Eo "\"html_url\": \"(.*?\/pull\/\\d+)\"," | sed -E "s/.*\"(https.*)\",/\\1/") || true
-echo "PR created: ${URL}"
--- ci/autoscaler/scripts/update-versions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./app-autoscaler-release-main/scripts/update_vendor_packages
