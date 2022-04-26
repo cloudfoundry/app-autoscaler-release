d852469518893a297282edf218035bb0669cc5a4
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2022-04-26 10:22:42 +0200

    add put step to push back commit changes

--- ci/autoscaler/tasks/update-sdk/create_pr.sh
+++ ci/autoscaler/tasks/update-sdk/create_pr.sh
@@ -33,7 +33,13 @@ pushd "${autoscaler_dir}" > /dev/null
   configure_git_credentials
 
   git checkout -b "${update_branch}"
+  git status
   git commit -a -m "${pr_title}"
-  echo "${github_token}" | gh auth login --with-token
+
+  echo "${GITHUB_PRIVATE_KEY}" >> ~/.ssh/id_rsa
+  ssh-add ~/.ssh/id_rsa
+
+  gh auth login
+
   gh pr create --base origin/main --title "${pr_title}" --body "${pr_description}"
 popd > /dev/null
