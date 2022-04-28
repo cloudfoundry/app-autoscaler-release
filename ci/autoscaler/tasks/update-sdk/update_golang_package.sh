cac923c93a643936a4e26f321a7dd7bf943da507
I551888 <susanne.salzmann@sap.com>
2022-04-28 15:23:18 +0200

    fixing typo

--- ci/autoscaler/tasks/update-sdk/update_golang_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_golang_package.sh
@@ -6,12 +6,12 @@ source "${script_dir}/vendor_package.sh"
 
 golang_version=$( cat "${autoscaler_dir}/packages/golang-1-linux/version")
 stripped_go_version=$(echo "${golang_version}" | cut -d . -f -2)
-echo ${golang_version} > version
+echo "${golang_version}" > version
 
 vendor-package golang-release golang-1-linux "${golang_version}"
 
 echo " - updating mod files with ${stripped_go_version}"
-find ${autoscaler_dir} -name go.mod -type f -exec sed -i '' "s/^[[:space:]]*go 1.*/go ${stripped_go_version}/g" "{}" \;
+find "${autoscaler_dir}" -name go.mod -type f -exec sed -i "s/^[[:space:]]*go 1.*/go ${stripped_go_version}/g" "{}" \;
 
 echo " - updating .tool-version with ${golang_version}"
 sed -i "s/golang 1.*/golang ${golang_version}/g" "${autoscaler_dir}/.tool-versions"
