577859a140df73025b67db4fc8b88f632fed5726
Kevin Cross <kevin.cross@sap.com>
2022-04-28 11:47:47 +0100

    Fix cat with echo

--- ci/autoscaler/tasks/update-sdk/update_java_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_java_package.sh
@@ -6,6 +6,6 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${script_dir}/vendor_package.sh"
 
 java_version=$(cat "${root_dir}/java-release/packages/openjdk-11/spec" | grep -e "- jdk-" | sed -E 's/- jdk-(.*)\.tar\.gz/\1/g')
-cat ${java_version} > version
+echo ${java_version} > version
 
 vendor-package java-release openjdk-11 "${java_version}"
