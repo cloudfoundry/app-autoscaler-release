0ba81a14275343e028620febb275cbe84f26f50e
Kevin Cross <kevin.cross@sap.com>
2022-05-09 09:13:34 +0100

    Changing the sha in the details to a md link for ease of viewing

--- ci/autoscaler/tasks/update-sdk/create_pr.sh
+++ ci/autoscaler/tasks/update-sdk/create_pr.sh
@@ -22,7 +22,7 @@ pushd "${autoscaler_dir}" > /dev/null
   dashed_version=$(echo "${package_version}" | sed -E 's/[._]/-/g' )
   update_branch="${type}-version-bump-${dashed_version}_${package_sha}"
   pr_title="Update ${type} version to ${package_version}"
-  pr_description="Automatic version bump of ${type} to \`${package_version}\`<br/>Package commit sha: \`${package_sha}\`"
+  pr_description="Automatic version bump of ${type} to \`${package_version}\`<br/>Package commit sha: [${package_sha}](https://github.com/bosh-packages/${type}-release/commit/${package_sha})"
 
   configure_git_credentials
 
