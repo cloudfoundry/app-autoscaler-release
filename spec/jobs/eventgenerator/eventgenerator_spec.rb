388e27ceed7de807ccfa3a60926d13244869f5a6
Alan Moran <alan.moran@sap.com>
2022-05-30 12:36:49 +0200

    Provides use_log_cache config to boshrelease and deployment

--- spec/jobs/eventgenerator/eventgenerator_spec.rb
+++ spec/jobs/eventgenerator/eventgenerator_spec.rb
@@ -38,19 +38,23 @@ describe "eventgenerator" do
   end
 
   context "config/eventgenerator.yml" do
+    let(:links) do
+      [
+        Bosh::Template::Test::Link.new(
+          name: "eventgenerator",
+          properties: {}
+        )
+      ]
+    end
+
+    let(:rendered_template) { YAML.safe_load(template.render(properties, consumes: links)) }
+
     it "does not set username nor password if not configured" do
       properties["autoscaler"]["eventgenerator"] = {
         "health" => {
           "port" => 1234
         }
       }
-      links = [
-        Bosh::Template::Test::Link.new(
-          name: "eventgenerator",
-          properties: {}
-        )
-      ]
-      rendered_template = YAML.safe_load(template.render(properties, consumes: links))
       expect(rendered_template["health"])
         .to include(
           {"port" => 1234}
@@ -65,19 +69,35 @@ describe "eventgenerator" do
           "password" => "test-user-password"
         }
       }
-      links = [
-        Bosh::Template::Test::Link.new(
-          name: "eventgenerator",
-          properties: {}
-        )
-      ]
-      rendered_template = YAML.safe_load(template.render(properties, consumes: links))
+
       expect(rendered_template["health"])
         .to include(
           {"port" => 1234,
            "username" => "test-user",
            "password" => "test-user-password"}
         )
+    end
+
+    describe "use_log_cache feature" do
+      it "keeps log cache off by default" do
+        expect(rendered_template['metricCollector'])
+          .to include({"use_log_cache" => false })
+      end
+
+      describe "when log cache on" do
+        before do
+          properties["autoscaler"]["eventgenerator"] = {
+            "metricscollector" => {
+              "use_log_cache" => true
+            }
+          }
+        end
+
+        it "check eventgenerator use log cache" do
+          expect(rendered_template['metricCollector'])
+            .to include({"use_log_cache" => true })
+        end
+      end
     end
   end
 end
