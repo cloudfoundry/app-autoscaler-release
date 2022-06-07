5d7a2003e1bd2cd6b2791346d85c4275f256eeaf
Kevin Cross <kevin.cross@sap.com>
2022-06-07 10:46:50 +0100

    extracting the version and deployment name ops files from the deploy-autoscaler script

--- ci/autoscaler/scripts/deploy-autoscaler.sh
+++ ci/autoscaler/scripts/deploy-autoscaler.sh
@@ -2,17 +2,14 @@
 
 set -euo pipefail
 
-VAR_DIR=bbl-state/bbl-state/vars
-
 system_domain="${SYSTEM_DOMAIN:-autoscaler.ci.cloudfoundry.org}"
 bbl_state_path="${BBL_STATE_PATH:-bbl-state/bbl-state}"
 deployment_name="${DEPLOYMENT_NAME:-app-autoscaler}"
-ops_files="${OPS_FILES:-()}"
+ops_files="${OPS_FILES:-''}"
 
-VAR_DIR=bbl-state/bbl-state/vars
-pushd ${bbl_state_path}
+pushd "${bbl_state_path}" > /dev/null
   eval "$(bbl print-env)"
-popd
+popd > /dev/null
 
 export UAA_CLIENT_SECRET=$(credhub get -n /bosh-autoscaler/cf/uaa_admin_client_secret --quiet)
 CF_ADMIN_PASSWORD=$(credhub get -n /bosh-autoscaler/cf/cf_admin_password -q)
@@ -25,6 +22,7 @@ exist=$(uaac client get autoscaler_client_id | grep -c NotFound)
 set -e
 
 function deploy () {
+  echo "Deploying Release"
   bosh -n -d ${deployment_name} \
     deploy templates/app-autoscaler-deployment.yml \
     ${OPS_FILES_TO_USE} \
@@ -51,6 +49,7 @@ fi
 
 pushd app-autoscaler-release
   # Determine if we need to upload a stemcell at this point.
+  #TODO refactor out function for stemcell check and update.
   STEMCELL_OS=$(yq eval '.stemcells[] | select(.alias == "default").os' templates/app-autoscaler-deployment.yml)
   STEMCELL_VERSION=$(yq eval '.stemcells[] | select(.alias == "default").version' templates/app-autoscaler-deployment.yml)
   STEMCELL_NAME="bosh-google-kvm-${STEMCELL_OS}-go_agent"
@@ -68,85 +67,45 @@ pushd app-autoscaler-release
   fi
 
   OPS_FILES_TO_USE=""
+  #NOTE: REQUIRED_OPS_FILES is a file in autoscaler-release
+  #TODO rename/replace REQUIRED_OPS_FILES with a variable or a propper file name (maybe required_ops_files.txt).
   if [ -f REQUIRED_OPS_FILES ]; then
     for OPS_FILE in $(cat REQUIRED_OPS_FILES); do
       if [ -f "${OPS_FILE}" ]; then
-        OPS_FILES_TO_USE="${OPS_FILES_TO_USE} -o ${OPS_FILE}"
-        #TODO: exit on else
+         OPS_FILES_TO_USE="${OPS_FILES_TO_USE} -o ${OPS_FILE}"
+      else
+        echo "ERROR: in REQUIRED_OPS_FILES could not find ops file ${OPS_FILE} in ${PWD}"
+        exit 1
       fi
     done
   fi
+
   for OPS_FILE in ${ops_files}; do
     if [ -f "${OPS_FILE}" ]; then
       OPS_FILES_TO_USE="${OPS_FILES_TO_USE} -o ${OPS_FILE}"
-      #TODO: exit on else
+    else
+      echo "ERROR: could not find ops file ${OPS_FILE} in ${PWD}"
+      exit 1
     fi
   done
 
-
   CURRENT_COMMIT_HASH=$(git log -1 --pretty=format:"%H")
-      cat << EOF > release_version.yml
----
-- type: replace
-  path: /releases/name=app-autoscaler/version?
-  value: ((app_autoscaler_version))
-EOF
 
-      cat << EOF > deployment_name.yml
----
-- type: replace
-  path: /name
-  value:  &deployment_name ((deployment_name))
-
-- type: replace
-  path: /variables/name=metricsserver_server/options/alternative_names
-  value:
-  - "metricsserver.service.cf.internal"
-  - "*.asmetrics.default.((deployment_name)).bosh"
-
-- type: replace
-  path: /instance_groups/jobs/name=route_registrar/properties/route_registrar/routes/name=api_server/uris
-  value:
-  - ((deployment_name)).((system_domain))
-
-- type: replace
-  path: /instance_groups/jobs/name=route_registrar/properties/route_registrar/routes/name=autoscaler_service_broker/uris
-  value:
-  - ((deployment_name))servicebroker.((system_domain))
-
-- type: replace
-  path: /instance_groups/jobs/name=route_registrar/properties/route_registrar/routes/name=autoscaler_metrics_forwarder/uris
-  value:
-  - ((deployment_name))metrics.((system_domain))
-
-- type: replace
-  path: /instance_groups/jobs/name=route_registrar/properties/route_registrar/routes/name=autoscaler_metricsforwarder_health/uris
-  value:
-  - ((deployment_name))-metricsforwarder.((system_domain))
-
-EOF
-
-  OPS_FILES_TO_USE="${OPS_FILES_TO_USE} -o release_version.yml -o deployment_name.yml"
   echo " - Using Ops files: '${OPS_FILES_TO_USE}'"
   set +e
   AUTOSCALER_EXISTS=$(bosh releases | grep -c "${CURRENT_COMMIT_HASH}")
   set -e
-  if [[ "${AUTOSCALER_EXISTS}" == 1 ]]; then
+
+  if [[ "${AUTOSCALER_EXISTS}" == 0 ]]; then
+    echo "Creating Release with bosh version ${CURRENT_COMMIT_HASH}"
+    bosh create-release --force --version=${CURRENT_COMMIT_HASH}
+
+    echo "Uploading Release"
+    bosh upload-release dev_releases/app-autoscaler/app-autoscaler-${CURRENT_COMMIT_HASH}.yml
+  else
     echo "the app-autoscaler release is already uploaded with the commit ${CURRENT_COMMIT_HASH}"
     echo "Attempting redeploy..."
-    deploy
-
-    exit 0
   fi
 
-  echo "Creating Release with bosh version ${CURRENT_COMMIT_HASH}"
-  bosh create-release --force --version=${CURRENT_COMMIT_HASH}
-
-  echo "Uploading Release"
-  bosh upload-release dev_releases/app-autoscaler/app-autoscaler-${CURRENT_COMMIT_HASH}.yml
-
-
-  echo "Deploying Release"
   deploy
-
 popd
