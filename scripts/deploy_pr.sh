1e01da86af467cc11187b7528cda1aaa86699069
Kevin Cross <kevin.cross@sap.com>
2022-06-08 14:38:12 +0100

    Moving to a single ops file instead

--- scripts/deploy_pr.sh
+++ scripts/deploy_pr.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 set -euo pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 root_dir=${script_dir}/../
-version=$(git log -1 --pretty=format:"%H")
+current_hash=$(git log -1 --pretty=format:"%H")
+version=${DEPLOYMENT_VERSION:-${current_hash}}
 deploy_name=${DEPLOYMENT_NAME:-app-autoscaler-12345}
 system_domain=autoscaler.ci.cloudfoundry.org
 bbl_state_path="${root_dir}/../app-autoscaler-env-bbl-state/bbl-state/"
@@ -10,12 +12,13 @@ bbl_state_path="${root_dir}/../app-autoscaler-env-bbl-state/bbl-state/"
 cd "${bbl_state_path}"
   eval "$(bbl print-env)"
 cd -
+
 password=$(credhub get -n /bosh-autoscaler/cf/cf_admin_password -q)
 
-credhub delete -p "/bosh-autoscaler/${deploy_name}"
+#credhub delete -p "/bosh-autoscaler/${deploy_name}"
 
 bosh -n -d ${deploy_name}\
- deploy --no-redact\
+ deploy --dry-run --no-redact\
  "${root_dir}/templates/app-autoscaler-deployment.yml"\
  -o "${root_dir}/example/operation/instance-identity-cert-from-cf.yml"\
  -o "${root_dir}/example/operation/enable-nats-tls.yml"\
@@ -29,12 +32,4 @@ bosh -n -d ${deploy_name}\
  -v admin_password="${password}"\
  -v cf_client_id=autoscaler_client_id\
  -v cf_client_secret=autoscaler_client_secret\
- -v skip_ssl_validation=true\
- -v postgres_domain="${deploy_name}.autoscalerpostgres.service.cf.internal"\
- -v api_domain="${deploy_name}.apiserver.service.cf.internal"\
- -v scheduler_domain="${deploy_name}.autoscalerscheduler.service.cf.internal"\
- -v servicebroker_domain="${deploy_name}.servicebroker.service.cf.internal"\
- -v eventgenerator_domain="${deploy_name}.eventgenerator.service.cf.internal"\
- -v scalingengine_domain="${deploy_name}.scalingengine.service.cf.internal"\
- -v metricsgateway_domain="${deploy_name}.metricsgateway.service.cf.internal"\
- -v metricsserver_domain="${deploy_name}.metricsserver.service.cf.internal"
+ -v skip_ssl_validation=true
