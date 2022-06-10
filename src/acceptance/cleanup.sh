408f254bb6614ffbeb9badf22d5092cac57bd2be
Marcin Kubica <marcin.kubica@sap.com>
2022-06-10 20:33:10 +0100

    fix: shellcheck

--- src/acceptance/cleanup.sh
+++ src/acceptance/cleanup.sh
@@ -7,16 +7,16 @@ deployment_name="${DEPLOYMENT_NAME:-app-autoscaler}"
 ORG_PREFIX="ASATS|ASUP|CUST_MET|TESTS-${deployment_name}"
 SERVICE_PREFIX=autoscaler
 
-ORGS=$(cf orgs | grep -v name | grep -E "${ORG_PREFIX}")
-for ORG in "$ORGS"; do
+ORGS=$(cf orgs | grep -v name | grep -E ${ORG_PREFIX})
+for ORG in $ORGS; do
 	set +e
 	cf delete-org "$ORG" -f
 	# shellcheck disable=SC2181
 	if [ "$?" != "0" ]; then
 		cf target -o "$ORG"
-		SERVICES=$(cf services | grep -v name | grep "${SERVICE_PREFIX}" | awk '{print $1}')
+		SERVICES=$(cf services | grep -v name | grep ${SERVICE_PREFIX} | awk '{print $1}')
 		for SERVICE in "$SERVICES"; do
-			cf purge-service-instance "$SERVICE" -f
+			cf purge-service-instance $SERVICE -f
 		done
 		cf delete-org -f "$ORG"
 	fi
