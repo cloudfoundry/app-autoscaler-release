81c7725725f2abf87e26e3166eda94dd8e71816a
Marcin Kubica <marcin.kubica@sap.com>
2022-06-10 17:35:36 +0100

    fix: var doublequotes

--- src/acceptance/cleanup.sh
+++ src/acceptance/cleanup.sh
@@ -7,15 +7,15 @@ deployment_name="${DEPLOYMENT_NAME:-app-autoscaler}"
 ORG_PREFIX="ASATS|ASUP|CUST_MET|TESTS-${deployment_name}"
 SERVICE_PREFIX=autoscaler
 
-ORGS=$(cf orgs  | grep -v name | grep -E ${ORG_PREFIX})
-for ORG in $ORGS; do
+ORGS=$(cf orgs | grep -v name | grep -E "${ORG_PREFIX}")
+for ORG in "$ORGS"; do
 	set +e
 	cf delete-org "$ORG" -f
 	# shellcheck disable=SC2181
 	if [ "$?" != "0" ]; then
 		cf target -o "$ORG"
-		SERVICES=$(cf services | grep -v name | grep ${SERVICE_PREFIX} | awk '{print $1}')
-		for SERVICE in $SERVICES; do
+		SERVICES=$(cf services | grep -v name | grep "${SERVICE_PREFIX}" | awk '{print $1}')
+		for SERVICE in "$SERVICES"; do
 			cf purge-service-instance "$SERVICE" -f
 		done
 		cf delete-org -f "$ORG"
