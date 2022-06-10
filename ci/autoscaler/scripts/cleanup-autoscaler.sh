15a04bf54df9f8d51c5aa0b9cfa7028dfa511b84
Kevin Cross <kevin.cross@sap.com>
2022-06-10 12:29:29 +0100

    adding multiple deployments into the cleanup script

--- ci/autoscaler/scripts/cleanup-autoscaler.sh
+++ ci/autoscaler/scripts/cleanup-autoscaler.sh
@@ -1,63 +1,35 @@
 #!/bin/bash
-
 set -euo pipefail
+system_domain="${SYSTEM_DOMAIN:-autoscaler.ci.cloudfoundry.org}"
+service_broker_name="${SERVICE_BROKER_NAME:-autoscalerservicebroker}"
+service_name="${SERVICE_NAME:-autoscaler}"
+deployment_name="${DEPLOYMENT_NAME:-app-autoscaler}"
+autoscaler_root=${AUTOSCALER_DIR:-app-autoscaler-release}
+bbl_state_path="${BBL_STATE_PATH:-bbl-state/bbl-state}"
 
-cf api https://api.${SYSTEM_DOMAIN} --skip-ssl-validation
+cf api "https://api.${system_domain}" --skip-ssl-validation
 
-pushd bbl-state/bbl-state
+pushd "${bbl_state_path}"
   eval "$(bbl print-env)"
 popd
 
 CF_ADMIN_PASSWORD=$(credhub get -n /bosh-autoscaler/cf/cf_admin_password -q)
-cf auth admin $CF_ADMIN_PASSWORD
+cf auth admin "$CF_ADMIN_PASSWORD"
 
-set +e
-pushd app-autoscaler-release/src/acceptance
+pushd "${autoscaler_root}/src/acceptance"
   ./cleanup.sh
 popd
-set -e
 
-set +e
-SERVICE_BROKER_EXISTS=$(cf service-brokers | grep -c autoscalerservicebroker.${SYSTEM_DOMAIN}) 
-set -e
+SERVICE_BROKER_EXISTS=$(cf service-brokers | grep -c "${service_broker_name}.${system_domain}")
 if [[ $SERVICE_BROKER_EXISTS == 1 ]]; then
   echo "Service Broker already exists, deleting..."
-  cf delete-service-broker autoscaler -f
+  cf delete-service-broker "${service_name}" -f
 fi
 
-set +e
-bosh delete-deployment -d app-autoscaler -n
-set -e
+bosh delete-deployment -d "${deployment_name}" -n
 
-set +e
-bosh delete-release app-autoscaler -n
-set -e
+#set +e
+#bosh delete-release "${deployment_name}" -n
+#set -e
 
-set +e
-credhub delete -n /bosh-autoscaler/app-autoscaler/postgres_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/postgres_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/metricsserver_client
-credhub delete -n /bosh-autoscaler/app-autoscaler/metricsserver_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/metricsserver_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/scheduler_client
-credhub delete -n /bosh-autoscaler/app-autoscaler/scheduler_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/scheduler_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_public_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_public_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_client
-credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/servicebroker_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_client
-credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_public_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_public_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/apiserver_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/eventgenerator_client
-credhub delete -n /bosh-autoscaler/app-autoscaler/eventgenerator_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/eventgenerator_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/scalingengine_client
-credhub delete -n /bosh-autoscaler/app-autoscaler/scalingengine_server
-credhub delete -n /bosh-autoscaler/app-autoscaler/scalingengine_ca
-credhub delete -n /bosh-autoscaler/app-autoscaler/autoscaler_service_broker_password
-credhub delete -n /bosh-autoscaler/app-autoscaler/database_password
-set -e
+credhub delete -p "/bosh-autoscaler/${deployment_name}"
