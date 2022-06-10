232c135607950130f677209a5279f8951b8748d6
Kevin Cross <kevin.cross@sap.com>
2022-06-10 14:32:12 +0100

    adding script to run acceptance tests locally

---
+++ act_acceptance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+export DEPLOYMENT_NAME="app-autoscaler-44"
+export SERVICE_BROKER_NAME="app-autoscaler-44servicebroker"
+export SERVICE_NAME="autoscaler-44"
+export BBL_STATE_PATH="../app-autoscaler-env-bbl-state/bbl-state"
+export SYSTEM_DOMAIN="autoscaler.ci.cloudfoundry.org"
+export AUTOSCALER_DIR="${PWD}"
+export CI_DIR="../app-autoscaler-ci/"
+export SERVICE_OFFERING_ENABLED=true
+export SKIP_SSL_VALIDATION=true
+export NAME_PREFIX="TESTS-${DEPLOYMENT_NAME}"
+export SUITES=api #"api app broker"
+export NODES=1
+${CI_DIR}/autoscaler/scripts/run-acceptance-tests.sh
