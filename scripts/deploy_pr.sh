645689444260b6d350763c3b49ec8b4ef15fb876
Kevin Cross <kevin.cross@sap.com>
2022-06-14 11:06:21 +0100

    update the hosts in the example file

--- scripts/deploy_pr.sh
+++ scripts/deploy_pr.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd "${script_dir}" > /dev/null
-source "./deploy_pr.sh"
+source "./pr-vars.source.sh"
 "${CI_DIR}/autoscaler/scripts/deploy-autoscaler.sh"
 "${CI_DIR}/autoscaler/scripts/register-broker.sh"
