5fe97b70ca7f4728f6a3954fa0f2add83404a1dc
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2022-07-06 17:27:32 +0200

    use ci from the app-autoscaler-release repo

--- ci/autoscaler/set-pipeline.sh
+++ ci/autoscaler/set-pipeline.sh
@@ -9,4 +9,4 @@ TARGET=${TARGET:-autoscaler}
 
 PIPELINE_NAME=app-autoscaler-release
 
-fly -t $TARGET set-pipeline --config=pipeline.yml --pipeline=$PIPELINE_NAME
+fly -t "$TARGET" set-pipeline --config=pipeline.yml --pipeline=$PIPELINE_NAME
