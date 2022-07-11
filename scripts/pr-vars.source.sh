075d820d1805a2682aab84011aa65962a8ff9c59
Marcin Kubica <marcin.kubica@sap.com>
2022-07-11 16:11:01 +0100

    Update new location for CI_DIR

--- scripts/pr-vars.source.sh
+++ scripts/pr-vars.source.sh
@@ -10,7 +10,7 @@ export SERVICE_NAME="autoscaler-${pr_number}"
 export BBL_STATE_PATH="${script_dir}/../../app-autoscaler-env-bbl-state/bbl-state"
 export SYSTEM_DOMAIN="autoscaler.ci.cloudfoundry.org"
 export AUTOSCALER_DIR="${script_dir}/../"
-export CI_DIR="${script_dir}/../../app-autoscaler-ci"
+export CI_DIR="${script_dir}/../ci"
 export OPS_FILES="${AUTOSCALER_DIR}/example/operation/loggregator-certs-from-cf.yml\
           ${AUTOSCALER_DIR}/example/operation/add-extra-plan.yml\
           ${AUTOSCALER_DIR}/example/operation/set-release-version.yml\
