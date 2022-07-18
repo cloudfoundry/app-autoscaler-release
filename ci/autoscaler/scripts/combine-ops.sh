8aff3ae19b612e43c42f7d275d59b320ef11290b
Alan Moran <alan.moran@sap.com>
2022-07-18 11:20:12 +0200

    Fix combined ops task for setup cf job
    Signed-off-by: Marcin Kubica <marcin.kubica@sap.com>

--- ci/autoscaler/scripts/combine-ops.sh
+++ ci/autoscaler/scripts/combine-ops.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 mkdir combined-ops/operations \
  && cp -r ops-files/operations/* combined-ops/operations \
- && cp -r custom-ops/operations/* combined-ops/operations
+ && cp -r custom-ops/ci/operations/* combined-ops/operations
