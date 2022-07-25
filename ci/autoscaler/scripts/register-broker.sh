942d734dd79df398f0324e181dec6ba771449619
Alan Moran <alan.moran@sap.com>
2022-07-25 16:36:21 +0200

    Makes register broker script idempontent

--- ci/autoscaler/scripts/register-broker.sh
+++ ci/autoscaler/scripts/register-broker.sh
@@ -13,20 +13,20 @@ popd
 
 cf api "https://api.${system_domain}" --skip-ssl-validation
 
-CF_ADMIN_PASSWORD=$(credhub get -n /bosh-autoscaler/cf/cf_admin_password -q)
-cf auth admin "${CF_ADMIN_PASSWORD}"
+cf_admin_password=$(credhub get -n /bosh-autoscaler/cf/cf_admin_password -q)
+cf auth admin "${cf_admin_password}"
 
 set +e
-SERVICE_BROKER_EXISTS=$(cf service-brokers | grep -c "${service_broker_name}.${system_domain}")
+service_broker_exists=$(cf service-brokers | grep -c "${service_broker_name}.${system_domain}")
 set -e
-if [[ "${SERVICE_BROKER_EXISTS}" == 1 ]]; then
+
+if [[ "${service_broker_exists}" == 1 ]]; then
   echo "Service Broker ${deployment_name} already exists, assuming this is ok..."
   cf delete-service-broker -f ${deployment_name}
-
-else
-  echo "Creating service broker ${deployment_name} at 'https://${service_broker_name}.${system_domain}'"
-  AUTOSCALER_SERVICE_BROKER_PASSWORD=$(credhub get  -n "/bosh-autoscaler/${deployment_name}/autoscaler_service_broker_password" -q)
-  cf create-service-broker "${deployment_name}" autoscaler_service_broker_user "$AUTOSCALER_SERVICE_BROKER_PASSWORD" "https://${service_broker_name}.${system_domain}"
 fi
+
+echo "Creating service broker ${deployment_name} at 'https://${service_broker_name}.${system_domain}'"
+autoscaler_service_broker_password=$(credhub get  -n "/bosh-autoscaler/${deployment_name}/autoscaler_service_broker_password" -q)
+cf create-service-broker "${deployment_name}" autoscaler_service_broker_user "$autoscaler_service_broker_password" "https://${service_broker_name}.${system_domain}"
 
 cf logout
