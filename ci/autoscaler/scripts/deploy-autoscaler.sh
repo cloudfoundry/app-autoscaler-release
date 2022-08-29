0f3ad2c8a03c013985647ece1046c0d78b1adb0b
Alan Moran <alan.moran@sap.com>
2022-08-29 20:38:03 +0200

    Enable fix bosh releases on autoscaler deploy

--- ci/autoscaler/scripts/deploy-autoscaler.sh
+++ ci/autoscaler/scripts/deploy-autoscaler.sh
@@ -9,6 +9,7 @@ bbl_state_path="${BBL_STATE_PATH:-bbl-state/bbl-state}"
 deployment_name="${DEPLOYMENT_NAME:-app-autoscaler}"
 autoscaler_dir="${AUTOSCALER_DIR:-app-autoscaler-release}"
 deployment_manifest="${script_dir}/../../../templates/app-autoscaler.yml"
+bosh_fix_releases="${BOSH_FIX_RELEASES:-false}"
 ops_files=${OPS_FILES:-"${autoscaler_dir}/operations/add-releases.yml\
  ${autoscaler_dir}/operations/instance-identity-cert-from-cf.yml\
  ${autoscaler_dir}/operations/add-postgres-variables.yml\
@@ -39,10 +40,17 @@ exist=$(uaac client get autoscaler_client_id | grep -c NotFound)
 set -e
 
 function deploy () {
+
+  if [[ $bosh_fix_releases == "true" ]]; then
+    bosh_fix_releases="${BOSH_FIX_RELEASES:-true}"
+    bosh_deploy_args="$bosh_args --fix-releases"
+  fi
+
   echo "# creating Bosh deployment '${deployment_name}' with version '${bosh_release_version}' in system domain '${system_domain}'   "
   bosh -n -d "${deployment_name}" \
     deploy "$deployment_manifest" \
     ${OPS_FILES_TO_USE} \
+    ${bosh_deploy_args} \
     -v system_domain="${system_domain}" \
     -v deployment_name="${deployment_name}" \
     -v app_autoscaler_version="${bosh_release_version}" \
