abc236d1cf59b1710004037402ab878b7603cc4f
Marcin Kubica <marcin.kubica@sap.com>
2022-08-30 10:28:58 +0100

    trigger buildin only on change in src/acceptance
    - use new system domain
    - remove obsolete dev scripts (moved to make)
    -- TODO: GINKO_OPS need a fix in dev mode

--- scripts/deploy_pr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-pushd "${script_dir}" > /dev/null
-source ./pr-vars.source.sh
-"${CI_DIR}/autoscaler/scripts/deploy-autoscaler.sh"
-#"${CI_DIR}/autoscaler/scripts/register-broker.sh"
