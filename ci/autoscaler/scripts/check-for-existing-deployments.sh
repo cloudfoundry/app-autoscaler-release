c4003faf3acb306e6240450371b3b434d3cd124e
Alan Moran <alan.moran@sap.com>
2022-08-31 12:19:06 +0200

    Adds fix for check for existing deployment step
    Signed-off-by: Arsalan Khan <muhammad.arsalan.khan@sap.com>

--- ci/autoscaler/scripts/check-for-existing-deployments.sh
+++ ci/autoscaler/scripts/check-for-existing-deployments.sh
@@ -2,8 +2,10 @@
 # shellcheck disable=SC2086
 set -euo pipefail
 
+[ -n "${DEBUG}" ] && set -x
 
 bbl_state_path="${BBL_STATE_PATH:-bbl-state/bbl-state}"
+
 
 pushd "${bbl_state_path}" > /dev/null
   eval "$(bbl print-env)"
@@ -12,7 +14,7 @@ popd > /dev/null
 
 deployment_count="$(bosh deployments --json | jq -r ".Tables[0].Rows | .[] | .name" | wc -l | sed 's/^ *//g')"
 
-if $deployment_count != "0"; then
+if [ "$deployment_count" != "0" ]; then
   echo "Cannot destroy infrastructure: delete $deployment_count existing deployments"
   exit 1
 fi
