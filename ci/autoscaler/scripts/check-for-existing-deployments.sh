a80fa3c38102262f78b6a0220117dd73f2513e7a
Alan Moran <alan.moran@sap.com>
2022-08-31 11:52:03 +0200

    Checks for exisiting deployments before destroying infra
    Signed-off-by: Arsalan Khan <muhammad.arsalan.khan@sap.com>

---
+++ ci/autoscaler/scripts/check-for-existing-deployments.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+set -euo pipefail
+
+
+bbl_state_path="${BBL_STATE_PATH:-bbl-state/bbl-state}"
+
+pushd "${bbl_state_path}" > /dev/null
+  eval "$(bbl print-env)"
+popd > /dev/null
+
+
+deployment_count="$(bosh deployments --json | jq -r ".Tables[0].Rows | .[] | .name" | wc -l | sed 's/^ *//g')"
+
+if $deployment_count != "0"; then
+  echo "Cannot destroy infrastructure: delete $deployment_count existing deployments"
+  exit 1
+fi
