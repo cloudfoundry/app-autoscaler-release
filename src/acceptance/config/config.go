07c157f5d6ec95fc9bb07cccd1432ced091373eb
Alan Moran <alan.moran@sap.com>
2022-09-02 12:54:52 +0200

    Fix run benchmark test
    Signed-off-by: Arsalan Khan <muhammad.arsalan.khan@sap.com>

--- src/acceptance/config/config.go
+++ src/acceptance/config/config.go
@@ -70,6 +70,7 @@ type Config struct {
 	// For bechmark test
 	BenchmarkAppCount          int `json:"benchmark_app_count"`
 	BenchmarkPercentageToScale int `json:"benchmark_app_percentage_to_scale"`
+	BenchmarkTeardown bool `json:"benchmark_teardown"`
 }
 
 var defaults = Config{
@@ -103,6 +104,7 @@ var defaults = Config{
 	// For benchmark
 	BenchmarkAppCount: 10,
 	BenchmarkPercentageToScale: 30,
+	BenchmarkTeardown: true,
 }
 
 func LoadConfig(t *testing.T) *Config {
