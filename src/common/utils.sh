eae7f6c8eda854518500200e41fb5cf3403dfec6
Kevin Cross <kevin.cross@sap.com>
2022-09-06 15:56:17 +0100

    Fix shellcheck warnings

--- src/common/utils.sh
+++ src/common/utils.sh
@@ -1,30 +1,31 @@
+#!/bin/bash
 
 # enable deep monitoring by relying on the rule set-up
 export DT_MONITOR="true"
 
-# set the number of file handles to a sensible 16k(base2)
-max_file_handles=16384
+# set the minimum number of file handles to a sensible 16k(base2)
+minimum_file_handles=16384
 
 mkdir -p /var/vcap/sys/log
-
-exec > >(tee -a >(logger -p user.info -t vcap.$(basename $0).stdout) | awk -W interactive '{ system("echo -n [$(date +\"%Y-%m-%d %H:%M:%S%z\")]"); print " " $0 }' >>/var/vcap/sys/log/$(basename $0).log)
-exec 2> >(tee -a >(logger -p user.error -t vcap.$(basename $0).stderr) | awk -W interactive '{ system("echo -n [$(date +\"%Y-%m-%d %H:%M:%S%z\")]"); print " " $0 }' >>/var/vcap/sys/log/$(basename $0).err.log)
+script_name=$(basename "$0")
+exec > >(tee -a >(logger -p user.info -t "vcap.${script_name}.stdout") | awk -W interactive '{ system("echo -n [$(date +\"%Y-%m-%d %H:%M:%S%z\")]"); print " " $0 }' >> "/var/vcap/sys/log/${script_name}.log")
+exec 2> >(tee -a >(logger -p user.error -t "vcap.${script_name}.stderr") | awk -W interactive '{ system("echo -n [$(date +\"%Y-%m-%d %H:%M:%S%z\")]"); print " " $0 }' >> "/var/vcap/sys/log/${script_name}.err.log")
 
 pid_guard() {
-  echo "------------ STARTING `basename $0` at `date` --------------" | tee /dev/stderr
+  echo "------------ STARTING ${script_name} at $(date) --------------" | tee /dev/stderr
   pidfile=$1
   name=$2
 
   if [ -f "$pidfile" ]; then
-    pid="$(head -1 $pidfile)"
-    echo "pidno $pid"
-    if [ -n "$pid" ] && [ -e "/proc/$pid" ] && grep -q "/var/vcap/packages/$name" "/proc/$pid/cmdline"; then
-      echo "$name is already running, please stop it first"
+    pid="$(head -1 "${pidfile}")"
+    echo "pidno ${pid}"
+    if [ -n "${pid}" ] && [ -e "/proc/$pid" ] && grep -q "/var/vcap/packages/$name" "/proc/$pid/cmdline"; then
+      echo "${name} is already running, please stop it first"
       exit 1
     fi
 
     echo "Removing stale pidfile..."
-    rm $pidfile
+    rm "${pidfile}"
   fi
 }
 
@@ -33,7 +34,7 @@ wait_pidfile() {
   try_kill=$2
   timeout=${3:-0}
   force=${4:-0}
-  countdown=$(( $timeout * 10 ))
+  countdown=$(( timeout * 10 ))
 
   if [ -f "$pidfile" ]; then
     pid=$(head -1 "$pidfile")
@@ -43,28 +44,28 @@ wait_pidfile() {
       exit 1
     fi
 
-    if [ -e /proc/$pid ]; then
+    if [ -e "/proc/${pid}" ]; then
       if [ "$try_kill" = "1" ]; then
         echo "Killing $pidfile: $pid "
-        kill $pid
+        kill "${pid}"
       fi
-      while [ -e /proc/$pid ]; do
+      while [ -e "/proc/${pid}" ]; do
         sleep 0.1
-        [ "$countdown" != '0' -a $(( $countdown % 10 )) = '0' ] && echo -n .
-        if [ $timeout -gt 0 ]; then
+        [ "$countdown" != '0' ] && [ $(( countdown % 10 )) = '0' ] && echo -n .
+        if [ "${timeout}" -gt 0 ]; then
           if [ $countdown -eq 0 ]; then
             if [ "$force" = "1" ]; then
               echo -ne "\nKill timed out, using kill -9 on $pid... "
-              kill -9 $pid
+              kill -9 "$pid"
               sleep 0.5
             fi
             break
           else
-            countdown=$(( $countdown - 1 ))
+            countdown=$(( countdown - 1 ))
           fi
         fi
       done
-      if [ -e /proc/$pid ]; then
+      if [ -e "/proc/$pid" ]; then
         echo "Timed Out"
       else
         echo "Stopped"
@@ -73,7 +74,7 @@ wait_pidfile() {
       echo "Process $pid is not running"
     fi
 
-    rm -f $pidfile
+    rm -f "$pidfile"
   else
     echo "Pidfile $pidfile doesn't exist"
   fi
@@ -86,9 +87,15 @@ kill_and_wait() {
   timeout=${2:-25}
   force=${3:-1}
 
-  wait_pidfile $pidfile 1 $timeout $force
+  wait_pidfile "${pidfile}" 1 "${timeout}" "${force}"
 }
 
 set_ulimits(){
-  ulimit -n ${max_file_handles}
+  #If the defaults of 1024 are set then use our default file limits
+  file_limit=$(ulimit -n)
+  if [ "${file_limit}" == "unlimited"  ]; then
+     return
+  elif [ "${file_limit}" -lt "${minimum_file_handles}" ]; then
+    ulimit -n ${minimum_file_handles}
+  fi
 }
