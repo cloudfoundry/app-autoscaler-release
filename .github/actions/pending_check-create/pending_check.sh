ce0338283d6872df15647cc7e388e07f2654eb50
Marcin Kubica <marcin.kubica@sap.com>
2022-09-07 11:33:23 +0100

    move inline bash script to file

---
+++ .github/actions/pending_check-create/pending_check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Checking run under ACT with override option
+eval "${ACT_RUN}"
+
+echo '::group::Creating new check'
+curl -vf -POST \
+   --retry 5 \
+   -H "Accept: application/vnd.github+json" \
+   -H "Authorization: token ${{ env.GITHUB_TOKEN }}" \
+   "https://api.github.com/repos/${{ env.GITHUB_REPOSITORY }}/check-runs" \
+-d '
+    {
+    "name":        "${{env.check_name}}",
+    "head_sha":    "${{env.PR_SHA}}",
+    "status":      "in_progress",
+    "external_id": "${{env.GH_RUN_ID}}",
+    "output": {
+                "title":   "${{ env.WORKFLOW_NAME }} running",
+                "summary": "pending check for commit ${{env.PR_SHA}}",
+                "text":    "Awaiting check result..."
+    }
+    }
+' \
+-o new_check.json
+
+id=$(jq -r '.id' new_check.json)
+
+if [ -z "${id}" ]; then
+  echo "ERROR: Failed to create the required check job"
+  echo "Result of curl:"
+  cat new_check.json
+  exit 1
+fi
+echo "::endgroup::"
+
+echo "Id is: ${id}"
