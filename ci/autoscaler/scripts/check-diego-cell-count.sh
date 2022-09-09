89644478af4f50908ef8a5343375d2afb019b145
Alan Moran <alan.moran@sap.com>
2022-09-09 15:34:19 +0200

    Adds missing inputs for benchmark

--- ci/autoscaler/scripts/check-diego-cell-count.sh
+++ ci/autoscaler/scripts/check-diego-cell-count.sh
@@ -5,10 +5,14 @@ set -euo pipefail
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 bbl_state_path="${BBL_STATE_PATH:-bbl-state/bbl-state}"
-actual_cells=$( bosh -d cf manifest | yq ".instance_groups | map(select(.name == \"diego-cell\")) | .[] | .instances")
 
 pushd "${bbl_state_path}" > /dev/null
   eval "$(bbl print-env)"
 popd > /dev/null
 
-[[ $expected_cells != $actual_cells ]] && exit 1
+actual_cells=$( bosh -d cf manifest | yq ".instance_groups | map(select(.name == \"diego-cell\")) | .[] | .instances")
+
+echo "Expeted diego cell count: $expected_cells"
+echo "Actual diego cell count: $actual_cells"
+
+[[ "$expected_cells" != "$actual_cells" ]] && exit 1
