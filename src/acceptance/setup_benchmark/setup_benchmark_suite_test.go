e1198d5a687d5d5ab503490c806df4a10369e086
Kevin Cross <kevin.cross@sap.com>
2022-09-12 10:46:17 +0100

    refactoring

--- src/acceptance/setup_benchmark/setup_benchmark_suite_test.go
+++ src/acceptance/setup_benchmark/setup_benchmark_suite_test.go
@@ -6,12 +6,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/KevinJCross/cf-test-helpers/v2/cf"
 	"github.com/KevinJCross/cf-test-helpers/v2/workflowhelpers"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gexec"
 )
 
 var (
@@ -36,19 +34,10 @@ var _ = BeforeSuite(func() {
 		orgs := helpers.GetTestOrgs(cfg)
 
 		for _, org := range orgs {
-			orgName, orgGuid, spaceName, spaceGuid := helpers.GetOrgSpaceNamesAndGuids(cfg, org)
+			orgName, _, spaceName, _ := helpers.GetOrgSpaceNamesAndGuids(cfg, org)
 			if spaceName != "" {
-				target := cf.Cf("target", "-o", orgName, "-s", spaceName).Wait(cfg.DefaultTimeoutDuration())
-				Expect(target).To(Exit(0), fmt.Sprintf("failed to target %s and %s", orgName, spaceName))
-
-				apps := helpers.GetApps(cfg, orgGuid, spaceGuid, "autoscaler-")
-				helpers.DeleteApps(cfg, apps, 0)
-
-				services := helpers.GetServices(cfg, orgGuid, spaceGuid, "autoscaler-")
-				helpers.DeleteServices(cfg, services)
+				helpers.DeleteOrg(cfg, orgName)
 			}
-
-			helpers.DeleteOrg(cfg, org)
 		}
 	})
 
