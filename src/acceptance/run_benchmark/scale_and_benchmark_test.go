bce66e6dd1a7285310a4b00b8670e24066c41d66
Kevin Cross <kevin.cross@sap.com>
2022-09-12 10:28:52 +0100

    Moving to using wait groups instead of chans

--- src/acceptance/run_benchmark/scale_and_benchmark_test.go
+++ src/acceptance/run_benchmark/scale_and_benchmark_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega/gmeasure"
 	"math"
+	"sync"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -15,18 +16,17 @@ import (
 var _ = Describe("Scale in and out (eg: 30%) percentage of apps", func() {
 	var (
 		appsToScaleCount int
-		samplingConfig gmeasure.SamplingConfig
-		experiment *gmeasure.Experiment
-
+		samplingConfig   gmeasure.SamplingConfig
+		experiment       *gmeasure.Experiment
 	)
 
 	BeforeEach(func() {
 		appsToScaleCount = int(math.RoundToEven(float64(cfg.BenchmarkAppCount * cfg.BenchmarkPercentageToScale / 100)))
 		ginkgo.GinkgoWriter.Printf("\nScaling %d app: \n", appsToScaleCount)
 		samplingConfig = gmeasure.SamplingConfig{
-			N: appsToScaleCount,
+			N:           appsToScaleCount,
 			NumParallel: appsToScaleCount,
-			Duration: 20 * time.Minute,
+			Duration:    20 * time.Minute,
 		}
 		experiment = gmeasure.NewExperiment("Scaling Benchmark")
 	})
@@ -39,47 +39,47 @@ var _ = Describe("Scale in and out (eg: 30%) percentage of apps", func() {
 	//	Expect(medianDuration).To(BeNumerically("~", 100*time.Millisecond, 50*time.Millisecond))
 	//})
 
-	Context("when scaling by custom metrics",func() {
+	Context("when scaling by custom metrics", func() {
 		It("should scale in", Serial, Label("measurement"), func() {
 			AddReportEntry(experiment.Name, experiment)
-
-
-			var scaledOut chan string
-			scaledOut = make(chan string,appsToScaleCount )
-
-			var scaledIn chan string
-			scaledIn = make(chan string,appsToScaleCount )
-
+			outer := sync.WaitGroup{}
+			outer.Add(appsToScaleCount)
 			experiment.Sample(func(i int) {
 				appName := fmt.Sprintf("node-custom-metric-benchmark-%d", i+1)
 				appGUID := helpers.GetAppGuid(cfg, appName)
 
+				//Scale out
+				wg := sync.WaitGroup{}
+				wg.Add(1)
 				experiment.MeasureDuration("scale-out", func() {
 					scaleOut := func() int {
 						helpers.SendMetric(cfg, appName, 550)
 						return helpers.RunningInstances(appGUID, 5*time.Second)
 					}
 					Eventually(scaleOut, 5*time.Minute, 15*time.Second).Should(Equal(2))
-					scaledOut <- appName
+					wg.Done()
+
 				})
+				wg.Wait()
 
-				Eventually(func() int {return len(scaledOut)},10* time.Minute, 5 * time.Second).Should(Equal(appsToScaleCount))
-
-				 experiment.MeasureDuration("scale-in", func() {
-					  scaleIn := func() int {
-						   helpers.SendMetric(cfg, appName, 100)
-						   //TODO: Change to autoscaler-app-history event instead of cf running instances.
-						   return helpers.RunningInstances(appGUID, 5*time.Second)
-					  }
-					 Eventually(scaleIn, 5*time.Minute, 15*time.Second).Should(Equal(1))
-					 scaledIn <- appName
-				 })
+				//Scale In
+				wg = sync.WaitGroup{}
+				wg.Add(1)
+				experiment.MeasureDuration("scale-in", func() {
+					scaleIn := func() int {
+						helpers.SendMetric(cfg, appName, 100)
+						//TODO: Change to autoscaler-app-history event instead of cf running instances.
+						return helpers.RunningInstances(appGUID, 5*time.Second)
+					}
+					Eventually(scaleIn).WithPolling(2 * time.Second).WithTimeout(5 * time.Minute).Should(Equal(1))
+					wg.Done()
+				})
+				wg.Wait()
+				outer.Done()
 			}, samplingConfig)
-
-			Eventually(func() int {return len(scaledIn)},10* time.Minute, 5 * time.Second).Should(Equal(appsToScaleCount))
+			outer.Wait()
 		})
 
 	})
-
 
 })
