41b8483039e6bc9cbcf7a06bc2de2300db37a271
Alan Moran <alan.moran@sap.com>
2022-09-14 14:45:30 +0200

    Fix combined ops files to include prometheus ops files

--- ci/infrastructure/scripts/combine-ops.sh
+++ ci/infrastructure/scripts/combine-ops.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 mkdir -p combined-ops/operations
 mkdir -p combined-ops/operations/cf
 mkdir -p combined-ops/operations/autoscaler
-mkdir -p combined-ops/operations/prometheus
+mkdir -p combined-ops/operations/prometheus/operators/cf
 
-cp -r cf-deployment/operations/* combined-ops/operations/cf/ \
-cp -r app-autoscaler-release/ci/operations/* combined-ops/autoscaler/operations
-cp -r prometheus-deployment/manifests/operators/* combined-ops/prometheus/operators
+cp -r cf-deployment/operations/* combined-ops/operations/cf/
+cp -r app-autoscaler-release/ci/operations/* combined-ops/operations/autoscaler
+cp -r prometheus-boshrelease/manifests/operators/cf/* combined-ops/operations/prometheus/operators/cf
