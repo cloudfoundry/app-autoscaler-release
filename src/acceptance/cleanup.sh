711868f954f07ce001725262ae2e5e61204b958f
Kevin Cross <kevin.cross@sap.com>
2022-09-28 17:19:47 +0100

    Added user cleanup to the acceptance cleanup

--- src/acceptance/cleanup.sh
+++ src/acceptance/cleanup.sh
@@ -12,18 +12,29 @@ function delete_org(){
     done
     cf delete-org -f "$ORG" || echo "ERROR: delete-org '$ORG' failed"
   fi
-  echo "Deleted $ORG"
+  echo " - deleted $ORG"
 }
 
 org_prefix=${NAME_PREFIX:-"ASATS|ASUP|CUST_MET"}
 SERVICE_PREFIX=autoscaler
 
 ORGS=$(cf orgs |  awk 'NR>3{ print $1}' | grep -E "${org_prefix}" || true)
-echo "Deleting orgs: '${ORGS}'"
+echo "# deleting orgs: '${ORGS}'"
 
 for ORG in $ORGS; do
 	# shellcheck disable=SC2181
   delete_org "$ORG" &
 done
+
+
+if [ -n "${org_prefix}" ]
+then
+  for user in $(cf curl /v3/users | jq -r '.resources[].username' | grep "${org_prefix}-" )
+  do
+    echo " - deleting left over user '${user}'"
+    cf delete-user -f "$user" &
+  done
+fi
+
 
 wait
