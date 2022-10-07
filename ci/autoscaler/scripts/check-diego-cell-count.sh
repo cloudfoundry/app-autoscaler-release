4880de7b74486b5290a4bdb1d17325f25e849c0f
Kevin Cross <kevin.cross@sap.com>
2022-10-07 17:42:18 +0100

    merging up refactor from main

--- ci/autoscaler/scripts/check-diego-cell-count.sh
+++ ci/autoscaler/scripts/check-diego-cell-count.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # shellcheck disable=SC2086
 set -euo pipefail
-
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-bbl_state_path="${BBL_STATE_PATH:-bbl-state/bbl-state}"
+source "${script_dir}/vars.source.sh"
 
 pushd "${bbl_state_path}" > /dev/null
   eval "$(bbl print-env)"
