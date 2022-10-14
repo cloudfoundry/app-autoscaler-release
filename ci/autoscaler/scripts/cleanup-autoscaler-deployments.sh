5b3d1748c9e933a81e745c46fc15f534d05e678d
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2022-10-14 13:44:11 +0200

    Sets debug
    Signed-off-by: Alan Moran <alan.moran@sap.com>

--- ci/autoscaler/scripts/cleanup-autoscaler-deployments.sh
+++ ci/autoscaler/scripts/cleanup-autoscaler-deployments.sh
@@ -5,7 +5,7 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${script_dir}/common.sh"
 
 function get_autoscaler_deployments(){
- cf service-brokers | cut -d' ' -f1 |grep autoscaler
+  cf service-brokers | cut -d' ' -f1 |grep autoscaler
 }
 
 function main(){
@@ -13,14 +13,17 @@ function main(){
   cf_login
 
   deployments=($(get_autoscaler_deployments))
+  set -x
   for deployment in "${deployments[@]}" ; do :
     export DEPLOYMENT_NAME="${deployment}"
     export NAME_PREFIX="${NAME_PREFIX:-"${DEPLOYMENT_NAME}-TESTS"}"
+
     cleanup_organization
     cleanup_service_broker
     cleanup_bosh_deployment
     cleanup_credhub
   done
+  set +x
 }
 
 main
