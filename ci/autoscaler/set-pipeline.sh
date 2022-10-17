d463e4a92f7e5f9d965674aa2f8bf14019d04160
Alan Moran <alan.moran@sap.com>
2022-10-17 11:17:42 +0200

    Pause all jobs but set-pipeline when configuring a non main pipeline
    Signed-off-by: Kevin Cross <kevin.cross@sap.com>

--- ci/autoscaler/set-pipeline.sh
+++ ci/autoscaler/set-pipeline.sh
@@ -7,21 +7,44 @@
 # Then  `TARGET=local set-pipeline.sh`
 set -euo pipefail
 
+TARGET="${TARGET:-autoscaler}"
+
 function set_pipeline(){
   local pipeline_name="$1"
+
   fly -t "${TARGET}" set-pipeline --config="pipeline.yml" --pipeline="${pipeline_name}" -v branch_name="${CURRENT_BRANCH}"
   fly -t autoscaler unpause-pipeline -p "${pipeline_name}"
 }
 
 function pause_job(){
   local job_name="$1"
+
   fly -t "${TARGET}" pause-job -j "${job_name}"
+}
+
+function unpause_job(){
+  local job_name="$1"
+
+  fly -t "${TARGET}" unpause-job -j "${job_name}"
+}
+
+function get_jobs(){
+  local pipeline_name="$1"
+
+  fly -t "${TARGET}" jobs --pipeline="${pipeline_name}" --json  | jq ".[].name" -r
+}
+
+function pause_jobs(){
+  local pipeline_name="$1"
+
+  for job in $(get_jobs "$pipeline_name"); do
+    pause_job "${pipeline_name}/$job"
+  done
 }
 
 function main(){
   SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
   pushd "${SCRIPT_RELATIVE_DIR}" > /dev/null
-    TARGET="${TARGET:-autoscaler}"
     CURRENT_BRANCH="$(git symbolic-ref --short HEAD)"
 
     if [[ "$CURRENT_BRANCH" == "main" ]];then
@@ -30,12 +53,8 @@ function main(){
     else
       export PIPELINE_NAME="app-autoscaler-release-${CURRENT_BRANCH}"
       set_pipeline "$PIPELINE_NAME"
-      pause_job "${PIPELINE_NAME}/release"
-      pause_job "${PIPELINE_NAME}/acceptance"
-      pause_job "${PIPELINE_NAME}/acceptance-buildin"
-      pause_job "${PIPELINE_NAME}/acceptance-log-cache"
-      pause_job "${PIPELINE_NAME}/upgrade-test"
-      pause_job "${PIPELINE_NAME}/cleanup-autoscaler-deployments"
+      pause_jobs "$PIPELINE_NAME"
+      unpause_job "$PIPELINE_NAME/set-pipeline"
     fi
 
 
