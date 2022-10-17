f6615052a6fb4709c5d783154b982ca8e2e4d64a
Alan Moran <alan.moran@sap.com>
2022-10-17 16:39:18 +0200

    spaces vs tabs
    Signed-off-by: Kevin Cross <kevin.cross@sap.com>

--- ci/autoscaler/scripts/common.sh
+++ ci/autoscaler/scripts/common.sh
@@ -4,10 +4,10 @@ source "${script_dir}/vars.source.sh"
 set -euo pipefail
 
 function bosh_login(){
-	if [[ ! -d ${bbl_state_path} ]]; then
-	  echo "FAILED: Did not find bbl-state folder at ${bbl_state_path}"
-	  echo "Make sure you have checked out the app-autoscaler-env-bbl-state repository next to the app-autoscaler-release repository to run this target or indicate its location via BBL_STATE_PATH";
-	  exit 1;
+  if [[ ! -d ${bbl_state_path} ]]; then
+    echo "FAILED: Did not find bbl-state folder at ${bbl_state_path}"
+    echo "Make sure you have checked out the app-autoscaler-env-bbl-state repository next to the app-autoscaler-release repository to run this target or indicate its location via BBL_STATE_PATH";
+    exit 1;
   fi
 
   pushd "${bbl_state_path}" > /dev/null
