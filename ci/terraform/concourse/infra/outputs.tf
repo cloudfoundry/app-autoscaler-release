19073178936627daf0909a95cc49c3bb4dd3db6f
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2022-10-24 16:20:44 +0200

    completed terragrunt infra

--- ci/terraform/concourse/infra/outputs.tf
+++ ci/terraform/concourse/infra/outputs.tf
@@ -1,11 +1,11 @@
-# output "load_balancer_ip" {
-#   # Consumed by concourse-app helm chart for Concourse
-#   value = module.concourse-infra.google_compute_address.concourse_app.address
-# }
+ output "load_balancer_ip" {
+   # Consumed by concourse-app helm chart for Concourse
+   value = google_compute_address.concourse_app.address
+ }
 
-# output "github_oauth" {
-#   # Consumed by Concourse github auth
-#   value     = module.concourse-infra.google_secret_manager_secret.github_oauth
-#   sensitive = true
-# }
+ output "github_oauth" {
+   # Consumed by Concourse github auth
+   value     = google_secret_manager_secret.github_oauth
+   sensitive = true
+ }
 
