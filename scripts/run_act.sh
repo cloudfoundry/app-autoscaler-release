c68324b9c8840a9fb8bb5b10214447b088d20114
Alan Moran <alan.moran@sap.com>
2022-10-26 12:30:52 +0200

    Disable cache for golangci lint errors

--- scripts/run_act.sh
+++ scripts/run_act.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# shellcheck disable=SC2012
+# shellcheck disable=SC2012,SC2068
 #
 workflows="$(ls .github/workflows  | cut -d'.' -f1)"
 
 
-select workflow in "${workflows[@]}"; do
+select workflow in ${workflows[@]}; do
   jobs=$(yq -r ".jobs | keys | .[]"  ".github/workflows/${workflow}.yaml")
-  select job in "${jobs[@]}"; do
-    act --workflows "./.github/workflows/${workflow}.yaml" --job "$job"
+  select job in ${jobs[@]}; do
+    act -r --workflows "./.github/workflows/${workflow}.yaml" --job "$job"
     break;
   done
+  break;
 done
