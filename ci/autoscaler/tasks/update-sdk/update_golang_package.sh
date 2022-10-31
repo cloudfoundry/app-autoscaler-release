467055385e82e493c3281d9ac8e65fb1b682557f
Kevin Cross <kevin.cross@sap.com>
2022-10-31 17:07:16 +0000

    Update java version to 17.0.4.1_1

--- ci/autoscaler/tasks/update-sdk/update_golang_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_golang_package.sh
@@ -3,18 +3,22 @@
 set -euo pipefail
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${script_dir}/vars.source.sh"
 source "${script_dir}/vendor_package.sh"
+export golang_dir=${GOLANG_DIR:-$(realpath -e "${autoscaler_dir}/../golang-release")}
+SED="sed"
+which gsed >/dev/null && SED=gsed
 
 # shellcheck disable=SC2154
-golang_version=$( cat "${root_dir}/golang-release/packages/golang-1-linux/version")
+golang_version=$( cat "${golang_dir}/packages/golang-1-linux/version")
 stripped_go_version=$(echo "${golang_version}" | cut -d . -f -2)
-echo -n "${golang_version}" > version
 
-vendor-package golang-release golang-1-linux "${golang_version}"
-
-echo " - updating mod files with ${stripped_go_version}"
+step "updating mod files with ${stripped_go_version}"
 # shellcheck disable=SC2154
-find "${autoscaler_dir}" -name go.mod -type f -exec sed -i "s/^[[:space:]]*go 1.*/go ${stripped_go_version}/g" "{}" \;
+find "${autoscaler_dir}" -name go.mod -type f -exec ${SED} -i "s/^[[:space:]]*go 1.*/go ${stripped_go_version}/g" "{}" \;
 
-echo " - updating .tool-version with ${golang_version}"
-sed -i "s/golang 1.*/golang ${golang_version}/g" "${autoscaler_dir}/.tool-versions"
+step "updating .tool-version with ${golang_version}"
+"${SED}" -i "s/golang 1.*/golang ${golang_version}/g" "${autoscaler_dir}/.tool-versions"
+
+echo -n "${golang_version}" > version
+vendor-package "$golang_dir" golang-1-linux "${golang_version}"
--- ci/autoscaler/tasks/update-sdk/update_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_package.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-workdir=${PWD}
+source "${script_dir}/vars.source.sh"
+create_pr=${CREATE_PR:-"false"}
+
 # shellcheck disable=SC2154
 "${script_dir}"/update_"${type}"_package.sh
-
-cd "${workdir}"
-"${script_dir}"/create_pr.sh
+[[ ${create_pr} == "true" ]] && "${script_dir}"/create_pr.sh
