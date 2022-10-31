b8fd333a500ac85ef4d38a7b4b4476f5b8acaec9
Kevin Cross <kevin.cross@sap.com>
2022-10-31 17:07:16 +0000

    update to java 17

--- ci/autoscaler/tasks/update-sdk/vendor_package.sh
+++ ci/autoscaler/tasks/update-sdk/vendor_package.sh
@@ -2,37 +2,38 @@
 
 set -euo pipefail
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-root_dir="${script_dir}/../../../../.."
-autoscaler_dir="${root_dir}/app-autoscaler-release"
+function write_vendor_commit(){
+  pushd "${release}" > /dev/null
+    git rev-parse HEAD > "${root_dir}/vendored-commit"
+  popd > /dev/null
+}
 
 function vendor-package {
   local release=${1}
   local package=${2}
   local version=${3}
   local package_location
-  package_location=${root_dir}/${release}
+  package_location=${release}
+  config_file=config/private.yml
+  log "Building package for ${release} for version '${version}'"
+  write_vendor_commit
 
-  echo "# Building package for ${release} for version '${version}'"
-  cat "${root_dir}/${release}/.git/ref" > "${root_dir}/vendored-commit"
-
-  pushd "${autoscaler_dir}" > /dev/null
     # generate the private.yml file with the credentials
-    echo "Generating private.yml..."
-    cat > config/private.yml <<EOF
+  step "Generating private.yml..."
+  cat > "${config_file}" <<EOF
 ---
 blobstore:
   options:
     credentials_source: static
     json_key:
 EOF
-    yq eval -i '.blobstore.options.json_key = strenv(UPLOADER_KEY)' config/private.yml
+  export UPLOADER_KEY=${UPLOADER_KEY:-$(cat "${HOME}/.ssh/autoscaler_blobstore_uploader.key")}
+  yq eval -i '.blobstore.options.json_key = strenv(UPLOADER_KEY)' "${config_file}"
 
-    bosh vendor-package "${package}" "${package_location}"
-    cp "${root_dir}/vendored-commit" "packages/${package}/vendored-commit" && git add "packages/${package}/vendored-commit"
-    echo -n "${version}" > "packages/${package}/version" && git add "packages/${package}/version"
+  bosh vendor-package "${package}" "${package_location}"
+  mv "${root_dir}/vendored-commit" "packages/${package}/vendored-commit" && git add "packages/${package}/vendored-commit"
+  mv "${root_dir}/version" "packages/${package}/version" && git add "packages/${package}/version"
 
-    echo "# Git diff -----"
-    git --no-pager diff
-  popd >/dev/null
+  log "Git diff -----"
+  git --no-pager diff
 }
