7c5aebf7c6415753b25b1e0463e9d1dc0144d405
Kevin Cross <kevin.cross@sap.com>
2022-11-01 09:45:30 +0000

    fix

--- ci/autoscaler/tasks/update-sdk/update_golang_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_golang_package.sh
@@ -18,7 +18,7 @@ vendor-package "$golang_dir" golang-1-linux "${golang_version}"
 
 echo " - updating mod files with ${stripped_go_version}"
 # shellcheck disable=SC2154
-find "${autoscaler_dir}" -name go.mod -type f -exec sed -i "s/^[[:space:]]*go 1.*/go ${stripped_go_version}/g" "{}" \;
+find "${autoscaler_dir}" -name go.mod -type f -exec ${SED} -i "s/^[[:space:]]*go 1.*/go ${stripped_go_version}/g" "{}" \;
 
 echo " - updating .tool-version with ${golang_version}"
 "${SED}" -i "s/golang 1.*/golang ${golang_version}/g" "${autoscaler_dir}/.tool-versions"
