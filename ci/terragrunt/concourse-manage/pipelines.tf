906a58203ed18810159fff56fe42656cc87a219f
Marcin Kubica <marcin.kubica@sap.com>
2022-11-02 14:29:08 +0100

    wip: move dr under concourse, try add cf infrastructure pipeline

--- ci/terragrunt/concourse-manage/pipelines.tf
+++ ci/terragrunt/concourse-manage/pipelines.tf
@@ -8,3 +8,14 @@ resource "concourse_pipeline" "api-tester" {
   pipeline_config        = file("pipelines/api-tester/pipeline.yml")
   pipeline_config_format = "yaml"
 }
+
+resource "concourse_pipeline" "infrastructure" {
+  team_name     = "app-autoscaler"
+  pipeline_name = "infrastructure"
+
+  is_exposed = true
+  is_paused  = true
+
+  pipeline_config        = file("../../infrastructure/pipeline.yml")
+  pipeline_config_format = "yaml"
+}
