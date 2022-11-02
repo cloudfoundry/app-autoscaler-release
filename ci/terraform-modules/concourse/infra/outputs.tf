2cc177b8198500fb81179597e06889a974920e16
Marcin Kubica <marcin.kubica@sap.com>
2022-11-02 09:19:06 +0100

    terraform - don't manage github secret

--- ci/terraform-modules/concourse/infra/outputs.tf
+++ ci/terraform-modules/concourse/infra/outputs.tf
@@ -7,9 +7,3 @@ output "load_balancer_dns" {
   # Consumed by concourse-app helm chart
   value = trimsuffix(google_dns_record_set.concourse.name, ".")
 }
-output "github_oauth_secret_name" {
-  # Consumed by Concourse github auth
-  value     = google_secret_manager_secret.github_oauth.name
-  sensitive = true
-}
-
