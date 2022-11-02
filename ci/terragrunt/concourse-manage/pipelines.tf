1c5368cef65dcae3d6654007ee71c3717eff618d
Marcin Kubica <marcin.kubica@sap.com>
2022-11-02 08:47:18 +0100

    revert to concourse 7.8.1 - pause api-tester pipeline

--- ci/terragrunt/concourse-manage/pipelines.tf
+++ ci/terragrunt/concourse-manage/pipelines.tf
@@ -1,20 +1,9 @@
-resource "concourse_pipeline" "hello_world" {
-  team_name     = "app-autoscaler"
-  pipeline_name = "hello-world"
-
-  is_exposed = true
-  is_paused  = false
-
-  pipeline_config        = file("pipelines/hello-world/pipeline.yml")
-  pipeline_config_format = "yaml"
-}
-
 resource "concourse_pipeline" "api-tester" {
   team_name     = "app-autoscaler"
   pipeline_name = "api-tester"
 
   is_exposed = true
-  is_paused  = false
+  is_paused  = true
 
   pipeline_config        = file("pipelines/api-tester/pipeline.yml")
   pipeline_config_format = "yaml"
