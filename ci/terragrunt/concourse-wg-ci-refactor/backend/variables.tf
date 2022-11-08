ba794116d516cf59d698ab529c7d8d5f02c0fa9d
Marcin Kubica <marcin.kubica@sap.com>
2022-11-08 09:08:47 +0000

    delete refactor wg-ci

--- ci/terragrunt/concourse-wg-ci-refactor/backend/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "project" { default = null }
-variable "region" { default = null }
-variable "zone" { default = null }
-
-variable "gke_name" { default = null }
-variable "sql_instance_name" { default = null }
