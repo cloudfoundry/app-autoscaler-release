0aa44544b19db9d210ba558c94a96f7931992c2c
Marcin Kubica <marcin.kubica@sap.com>
2022-11-09 16:23:32 +0000

    add initial autoscaler gcs buckets and service accounts

--- ci/terragrunt/concourse-app-autoscaler/variables.tf
+++ ci/terragrunt/concourse-app-autoscaler/variables.tf
@@ -1,3 +1,11 @@
 variable "fly_target" {
     default = "app-runtime-interfaces"
 }
+
+variable "project" {
+    default = "app-runtime-interfaces-wg"
+}
+
+variable "region" {
+    default = "europe-west3"
+}
