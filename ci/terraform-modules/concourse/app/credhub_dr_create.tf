03812afe9f00064a73f517f54de5579417cf6bfe
Oliver Mautschke <oliver.mautschke@sap.com>
2022-11-10 11:18:36 +0100

    Migrate dr-create into app

--- ci/terraform-modules/concourse/app/credhub_dr_create.tf
+++ ci/terraform-modules/concourse/app/credhub_dr_create.tf
@@ -1,4 +1,21 @@
-# Save encryption key once app is deployed
+resource "google_secret_manager_secret" "credhub_encryption_key" {
+  secret_id = "${var.gke_name}-credhub-encryption-key"
+  project   = var.project
+  # when creating versions with gcloud it creates empty labels
+  labels = {
+  }
+
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+
+  depends_on = [carvel_kapp.concourse_app]
+}
+
 data "kubernetes_secret_v1" "credhub_encryption_key" {
   metadata {
     name      = "credhub-encryption-key"
@@ -7,35 +24,21 @@ data "kubernetes_secret_v1" "credhub_encryption_key" {
   binary_data = {
     password = ""
   }
-}
 
-resource "google_secret_manager_secret" "credhub_encryption_key" {
-  secret_id = "${var.gke_name}-credhub-encryption-key"
-  project   = var.project
-  # when creating versions with gcloud it creates empty labels
-  labels = {
-  }
-  replication {
-    user_managed {
-      replicas {
-        location = var.region
-      }
-    }
-  }
+  depends_on = [carvel_kapp.concourse_app]
 }
-
 
 resource "google_secret_manager_secret_version" "credhub_encryption_key" {
   secret      = google_secret_manager_secret.credhub_encryption_key.id
   secret_data = base64decode(data.kubernetes_secret_v1.credhub_encryption_key.binary_data.password)
   lifecycle {
+    prevent_destroy = true
+
     # If omitted or unset terraform destroys previous versions which will make it impossible to
     # restore them. This is relevant in case of a desaster recovery where the
     # history of secret might be needed to restore all credhub secrets.
     #
     # See: https://github.com/hashicorp/terraform-provider-google/issues/8653
-    prevent_destroy = true
-
     # Terraform will retrun error if user will attempt to create new version of credhub encryption key.
     # Such scenario should be a red flag and performed only when fully accepting the consequences of creating new
     #   secret version with terraform - and that is a permanent destruction of the previously saved secret.
@@ -44,3 +47,11 @@ resource "google_secret_manager_secret_version" "credhub_encryption_key" {
   }
 }
 
+module "assertion_encryption_key_identical" {
+  source  = "Invicton-Labs/assertion/null"
+
+  condition = google_secret_manager_secret_version.credhub_encryption_key.secret_data == base64decode(data.kubernetes_secret_v1.credhub_encryption_key.binary_data.password)
+
+  // The error message to print out if the condition evaluates to FALSE
+  error_message = "*** Encryption keys in terraform and kubernetes do not match ***"
+}
