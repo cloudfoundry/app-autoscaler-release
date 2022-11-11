f91b44c50fd56c0c8d971a8e629d5f3453d2d232
Alan Moran <alan.moran@sap.com>
2022-11-11 12:40:17 +0100

    Fix shellcheck

--- ci/autoscaler/set-pipeline.sh
+++ ci/autoscaler/set-pipeline.sh
@@ -6,8 +6,8 @@
 # When running concourse locally: ` fly -t "local" login -c "http://localhost:8080" `
 # Then  `TARGET=local set-pipeline.sh`
 set -euo pipefail
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/scripts
-source "${script_dir}/vars.source.sh"
+
+export PR_NUMBER=${PR_NUMBER:-$(gh pr view --json number --jq '.number' )}
 
 fly_args=""
 
@@ -33,7 +33,7 @@ function set_pipeline(){
   fi
 
   # shellcheck disable=SC2086
-  fly -t "${TARGET}" set-pipeline --config="pipeline.yml"--pipeline="${pipeline_name}" $fly_args
+  fly -t "${TARGET}" set-pipeline --config="pipeline.yml" --pipeline="${pipeline_name}" $fly_args
 
   fly -t autoscaler unpause-pipeline -p "${pipeline_name}"
 }
