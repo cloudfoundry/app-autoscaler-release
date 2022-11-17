ee61dda2253b802bace517db9c4af5e4856257de
Jörg Weisbarth <joerg.weisbarth@sap.com>
2022-11-17 15:27:23 +0100

    Remove debug-code;

--- ci/autoscaler/scripts/deploy-autoscaler.sh
+++ ci/autoscaler/scripts/deploy-autoscaler.sh
@@ -96,7 +96,6 @@ function deploy() {
 
   local tmp_manifest_file
   tmp_manifest_file="$(mktemp --tmpdir="${tmp_dir}" "${deployment_name}.bosh-manifest.yaml.XXX")"
-  echo '¡¡¡1!!!'; ls -lah "${tmp_manifest_file}" #; cat "${tmp_manifest_file}" # TODO: Debug github action
   bosh -n -d "${deployment_name}" \
       interpolate "${deployment_manifest}" \
       ${OPS_FILES_TO_USE} \
@@ -109,7 +108,6 @@ function deploy() {
       -v cf_client_secret=autoscaler_client_secret \
       -v skip_ssl_validation=true \
       > "${tmp_manifest_file}"
-  echo '¡¡¡2!!!'; ls -lah "${tmp_manifest_file}" #; cat "${tmp_manifest_file}" # TODO: Debug github action
   if [ -z "${DEBUG+}" ] && [ "${DEBUG}" != 'false' ]
   then
     echo "Manifest for '${deployment_name}' to deploy with bosh written into file ${tmp_manifest_file}."
@@ -124,7 +122,6 @@ function deploy() {
 
   echo "> creating Bosh deployment '${deployment_name}' with version '${bosh_release_version}' in system domain '${system_domain}'   "
   echo " - tmp_manifest_file=${tmp_manifest_file}"
-  echo '¡¡¡3!!!'; ls -lah "${tmp_manifest_file}"; cat "${tmp_manifest_file}" # TODO: Debug github action
   echo " - Using Ops files: '${OPS_FILES_TO_USE}'"
   echo " - Deploy options: '${bosh_deploy_opts}'"
   bosh -n -d "${deployment_name}" deploy "${tmp_manifest_file}"
