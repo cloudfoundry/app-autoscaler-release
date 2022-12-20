916ef1826bc21b7a00c18e8f1e0a2c80424f82eb
Jörg Weisbarth <joerg.weisbarth@sap.com>
2022-12-20 14:51:01 +0100

    Remove personal configuration;

--- act.sh
+++
@@ -1,8 +0,0 @@
-ACCEPTANCE_TEST_SECRET_FILE="~/.bbl_ssh_key"
-GITHUB_TOKEN="$(cat ~/.github_token)"
-
-act --workflows ./.github/workflows/acceptance_tests_broker.yaml \
-   --job acceptance_tests_reusable\
-   --eventpath .github/test/event.json \
-   --secret-file "${ACCEPTANCE_TEST_SECRET_FILE}" \
-   --secret GITHUB_TOKEN="${GITHUB_TOKEN}"
