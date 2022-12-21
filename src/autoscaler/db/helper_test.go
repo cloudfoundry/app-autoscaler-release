4ead045245914390be00aebbe526d9996104972d
Marcin Kubica <marcin.kubica@sap.com>
2022-12-21 13:08:25 +0100

    wip try env host parameter for ginkgo mysql tests

--- src/autoscaler/db/helper_test.go
+++ src/autoscaler/db/helper_test.go
@@ -22,26 +22,26 @@ var _ = Describe("Helper", func() {
 		})
 		Context("when mysql query parameters are provided", func() {
 			BeforeEach(func() {
-				dbUrl = "root@tcp(localhost:3306)/autoscaler?tls=preferred"
+				dbUrl = "root@tcp(" + os.Getenv("DB_HOST") + ":3306)/autoscaler?tls=preferred"
 			})
 			It("returns mysql database object", func() {
 				Expect(err).NotTo(HaveOccurred())
 				Expect(database).To(Equal(&Database{
 					DriverName: "mysql",
-					DSN:        "root@tcp(localhost:3306)/autoscaler?parseTime=true&tls=preferred",
+					DSN:        "root@tcp(" + os.Getenv("DB_HOST") + ":3306)/autoscaler?parseTime=true&tls=preferred",
 				}))
 			})
 		})
 
 		Context("when mysql query parameters are not provided", func() {
 			BeforeEach(func() {
-				dbUrl = "root@tcp(localhost:3306)/autoscaler"
+				dbUrl = "root@tcp(" + os.Getenv("DB_HOST") + ":3306)/autoscaler"
 			})
 			It("returns mysql database object", func() {
 				Expect(err).NotTo(HaveOccurred())
 				Expect(database).To(Equal(&Database{
 					DriverName: "mysql",
-					DSN:        "root@tcp(localhost:3306)/autoscaler?parseTime=true",
+					DSN:        "root@tcp(" + os.Getenv("DB_HOST") + ":3306)/autoscaler?parseTime=true",
 				}))
 			})
 
@@ -50,20 +50,20 @@ var _ = Describe("Helper", func() {
 		Context("when need to verify mysql server, cert is provided ", func() {
 			BeforeEach(func() {
 				certPath = "../../../test-certs/api.crt"
-				dbUrl = "root@tcp(localhost:3306)/autoscaler?tls=verify-ca&sslrootcert=" + certPath
+				dbUrl = "root@tcp" + os.Getenv("DB_HOST") + ":3306)/autoscaler?tls=verify-ca&sslrootcert=" + certPath
 			})
 			It("returns mysql database connection", func() {
 				Expect(err).NotTo(HaveOccurred())
 				Expect(database).To(Equal(&Database{
 					DriverName: "mysql",
-					DSN:        "root@tcp(localhost:3306)/autoscaler?parseTime=true&tls=verify-ca",
+					DSN:        "root@tcp(" + os.Getenv("DB_HOST") + ":3306)/autoscaler?parseTime=true&tls=verify-ca",
 				}))
 			})
 		})
 
 		Context("when need to verify mysql server, cert is not provided ", func() {
 			BeforeEach(func() {
-				dbUrl = "root@tcp(localhost:3306)/autoscaler?tls=verify-ca"
+				dbUrl = "root@tcp(" + os.Getenv("DB_HOST") + ":3306)/autoscaler?tls=verify-ca"
 			})
 			It("should error", func() {
 				Expect(err).To(HaveOccurred())
