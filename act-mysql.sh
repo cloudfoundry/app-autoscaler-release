364f968f42184838feb7861419fc9d65680f2c6d
Marcin Kubica <marcin.kubica@sap.com>
2022-12-22 14:47:10 +0100

    use DB_HOST var for postgres java tests

--- act-mysql.sh
+++
@@ -1,8 +0,0 @@
-ACCEPTANCE_TEST_SECRET_FILE="~/.bbl_ssh_key"
-GITHUB_TOKEN="$(cat ~/.github_token)"
-
-act --workflows ./.github/workflows/mysql.yaml \
-   --job build \
-   --eventpath .github/test/event.json \
-   --secret-file "${ACCEPTANCE_TEST_SECRET_FILE}" \
-   --secret GITHUB_TOKEN="${GITHUB_TOKEN}"
