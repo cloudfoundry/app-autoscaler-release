09d6dc51d4726a4030b6bbdf8604a54fc23534a8
Jörg Weisbarth <joerg.weisbarth@sap.com>
2023-03-14 13:22:20 +0100

    Sonarcloud should avoid test-files;
    Add comment for importance of the line;

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
               #
               # jdwpkgs.rubyPackages.cf-uaac
               shellcheck
+              sonar-scanner-cli
               temurin-bin
               yq-go
             ];
