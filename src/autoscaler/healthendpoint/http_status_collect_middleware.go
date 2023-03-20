1ce8edb3a344d4b3630f8ca4694c42dd39424313
Jörg Weisbarth <joerg.weisbarth@sap.com>
2023-03-20 17:25:07 +0100

    Remove wrong health-endpoint from api-server.
    Changes to get 'make test' running
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    commenting out 3 of 4/5 Collect statements for debugging
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    commenting out 4 of 4/5 Collect statements for debugging
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    adding print statements and commenting out 3 of 4/5 Collect statements for debugging
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    adding more print statements
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    adding more print statements
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    adding more print statements
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    adding more print statements
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    adding more debug print statements
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    Revert "adding more debug print statements"
    
    This reverts commit eefc74f4173c29ea5c9d747209d1a23b7aa22f6a.
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    Revert "adding more print statements"
    
    This reverts commit 958fd001c7f92ad2513847e7c86366cce081ba19.
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    Revert "adding more print statements"
    
    This reverts commit 0b2d81bbd9dacfb2284f335f336445266904cfda.
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    Revert "adding more print statements"
    
    This reverts commit a25e782f4294ae810936424ff85a9f53db14ba90.
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    Revert "adding more print statements"
    
    This reverts commit d433ce51e609751e22d6414bcb6480928e23f63f.
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>
    
    Adding print statements of status collect middleware to stderr
    
    Co-authored-by: Susanne Salzmann <susanne.salzmann@sap.com>

--- src/autoscaler/healthendpoint/http_status_collect_middleware.go
+++ src/autoscaler/healthendpoint/http_status_collect_middleware.go
@@ -1,7 +1,9 @@
 package healthendpoint
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 type HTTPStatusCollectMiddleware struct {
@@ -18,6 +20,9 @@ func (h *HTTPStatusCollectMiddleware) Collect(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		h.httpStatusCollector.IncConcurrentHTTPRequest()
 		defer h.httpStatusCollector.DecConcurrentHTTPRequest()
+		fmt.Fprintf(os.Stderr, "\n\nCHECKING w\n\n %v \n", w)
+		fmt.Fprintf(os.Stderr, "\n\nCHECKING r\n\n %v \n", r)
+		fmt.Fprintf(os.Stderr, "\n\nnext\n\n %v \n", next)
 		next.ServeHTTP(w, r)
 	})
 }
