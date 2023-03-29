2b4d538775f66a6fb57f820e2cd7cfa8d9fda891
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2023-03-29 15:19:12 +0200

    golangAPIServer => add Bosh template test

--- src/autoscaler/healthendpoint/http_status_collect_middleware.go
+++ src/autoscaler/healthendpoint/http_status_collect_middleware.go
@@ -1,9 +1,7 @@
 package healthendpoint
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 )
 
 type HTTPStatusCollectMiddleware struct {
@@ -20,9 +18,6 @@ func (h *HTTPStatusCollectMiddleware) Collect(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		h.httpStatusCollector.IncConcurrentHTTPRequest()
 		defer h.httpStatusCollector.DecConcurrentHTTPRequest()
-		fmt.Fprintf(os.Stderr, "\n\nCHECKING w\n\n %v \n", w)
-		fmt.Fprintf(os.Stderr, "\n\nCHECKING r\n\n %v \n", r)
-		fmt.Fprintf(os.Stderr, "\n\nnext\n\n %v \n", next)
 		next.ServeHTTP(w, r)
 	})
 }
