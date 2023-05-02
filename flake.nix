f1f1876e86f593d88d4ac2f63e19dad7d73d7117
Jörg Weisbarth <joerg.weisbarth@sap.com>
2023-05-02 15:22:33 +0200

    Set test-app to statically linked by default;

--- flake.nix
+++ flake.nix
@@ -35,7 +35,10 @@
               google-cloud-sdk
               maven
               nodejs
-              patchelf # We use the binary-buildpack and nix-build-results have set the wrong ELF-Interpreter.
+              # We use the binary-buildpack and nix-build-results have set the wrong ELF-Interpreter.
+              # For more background, see:
+              # <https://blog.thalheim.io/2022/12/31/nix-ld-a-clean-solution-for-issues-with-pre-compiled-executables-on-nixos>
+              patchelf
               ruby
               # The following package for cf-uaac is needed by our makefile as well.
               # Until PR https://github.com/NixOS/nixpkgs/pull/189079 has been merged, this requires
