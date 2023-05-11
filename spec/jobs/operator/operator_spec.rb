a7ba83bd188ee9bf1dced276947b017a8e7fc749
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2023-05-11 19:03:43 +0200

    operator-> Bosh Template Specs

--- spec/jobs/operator/operator_spec.rb
+++ spec/jobs/operator/operator_spec.rb
@@ -12,36 +12,6 @@ describe "operator" do
   let(:rendered_template) { YAML.safe_load(template.render(properties)) }
 
   context "config/operator.yml" do
-    it "does not set username nor password if not configured" do
-      properties["autoscaler"]["operator"] = {
-        "health" => {
-          "port" => 1234
-        }
-      }
-
-      expect(rendered_template["health"])
-        .to include(
-          {"port" => 1234}
-        )
-    end
-
-    it "check operator basic auth username and password" do
-      properties["autoscaler"]["operator"] = {
-        "health" => {
-          "port" => 1234,
-          "username" => "test-user",
-          "password" => "test-user-password"
-        }
-      }
-
-      expect(rendered_template["health"])
-        .to include(
-          {"port" => 1234,
-           "username" => "test-user",
-           "password" => "test-user-password"}
-        )
-    end
-
     context "uses tls" do
       context "policy_db" do
         it "includes the ca, cert and key in url when configured" do
@@ -116,6 +86,33 @@ describe "operator" do
             check_if_certs_not_in_url(url, "lock_db")
           end
         end
+      end
+    end
+
+    context "health_server" do
+      it "default port exist" do
+        expect(rendered_template["health"]["port"]).to eq(6208)
+      end
+
+      it "credentials are defined" do
+        expect(rendered_template["health"]["username"]).to eq("basic_auth_username")
+        expect(rendered_template["health"]["password"]).to eq("basic_auth_secret")
+      end
+
+      it "readiness enabled is set to false by default" do
+        expect(rendered_template["health"]["readiness_enabled"]).to eq(false)
+      end
+
+      it "unprotected_endpoint config is empty by default" do
+        expect(rendered_template["health"]["unprotected_endpoints"]).to match_array([])
+      end
+
+      it "has valid endpoints in unprotected_endpoint config" do
+        properties["autoscaler"]["operator"]["health"]["unprotected_endpoints"] = %w[/debug/pprof /health/liveness /health/prometheus /health/readiness]
+        expect(rendered_template["health"]["unprotected_endpoints"]).to contain_exactly("/health/liveness",
+          "/health/prometheus",
+          "/health/readiness",
+          "/debug/pprof")
       end
     end
   end
