6b49f0f3d3e0a96df5546271c1c2589500d48e3a
Jörg Weisbarth <joerg.weisbarth@sap.com>
2023-08-14 12:24:34 +0200

    Add production of intermediate files and integrate it into the Makefile;

--- src/acceptance/assets/app/go_app/internal/generate.go
+++ src/acceptance/assets/app/go_app/internal/generate.go
@@ -1,6 +1,6 @@
 package internal
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --no-server --debug.ignoreNotImplemented "mutualTLS security" --target custommetrics --clean ../../../../../../api/custom-metrics-api.yaml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen  --no-server --debug.ignoreNotImplemented "mutualTLS security" --target scalinghistory --clean ../../../../../../api/scaling-history-api.yaml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen  --no-server --debug.ignoreNotImplemented "mutualTLS security" --target policy --clean ../../../../../../api/policy-api.yaml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen  --no-server --debug.ignoreNotImplemented "mutualTLS security" --target applicationmetric --clean ../../../../../../api/application-metric-api.yaml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --no-server --debug.ignoreNotImplemented "mutualTLS security" --target applicationmetric --clean ../../../../../../api/application-metric-api.openapi.yaml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --no-server --debug.ignoreNotImplemented "mutualTLS security" --target custommetrics --clean ../../../../../../api/custom-metrics-api.openapi.yaml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --no-server --debug.ignoreNotImplemented "mutualTLS security" --target policy --clean ../../../../../../api/policy-api.openapi.yaml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --no-server --debug.ignoreNotImplemented "mutualTLS security" --target scalinghistory --clean ../../../../../../api/scaling-history-api.openapi.yaml
