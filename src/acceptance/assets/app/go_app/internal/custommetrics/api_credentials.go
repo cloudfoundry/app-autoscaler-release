f5ad36928ad7682db89f4700415536998389cf32
Jörg Weisbarth <joerg.weisbarth@sap.com>
2023-08-14 10:59:28 +0200

    Remove generated code;

--- src/acceptance/assets/app/go_app/internal/custommetrics/api_credentials.go
+++
@@ -1,8 +0,0 @@
-package api
-
-type CustomMetricsCredentials struct {
-	Username string `mapstructure:"username" json:"username"`
-	Password string `mapstructure:"password" json:"password"`
-	URL      string `mapstructure:"url" json:"url"`
-	MtlsURL  string `mapstructure:"mtls_url" json:"mtls_url"`
-}
