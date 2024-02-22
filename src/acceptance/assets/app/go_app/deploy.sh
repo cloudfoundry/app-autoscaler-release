f2e4ff6726ff8a9f1a62685e514de8001c62ca87
Josua Geiger <josua.geiger@sap.com>
2024-02-22 09:15:45 +0100

    fix `make deploy`

--- src/acceptance/assets/app/go_app/deploy.sh
+++ src/acceptance/assets/app/go_app/deploy.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
-
 set -euo pipefail
-config=${CONFIG:-}
+
+config="$(cat "${CONFIG:-}")"
+
 function getConfItem(){
-  val=$(jq -r ".$1" "${config}")
+  val=$(jq -r ".$1" <<< "${config}")
   if [ "$val" = "null" ]; then return 1; fi
   echo "$val"
 }
+
 if [ -z "${config}" ]; then
   echo "ERROR: Please supply the config using CONFIG env variable"
   exit 1
 fi
 
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-app_dir="$( realpath -e "${script_dir}/build")"
-service_name=$(getConfItem service_name)
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-cf create-org test
-cf target -o test
-cf create-space test_app
-cf target -s test_app
+app_dir="$(realpath -e "${script_dir}/build")"
+app_domain="$(getConfItem apps_domain)"
+service_name="$(getConfItem service_name)"
+memory_mb="$(getConfItem node_memory_limit||echo 128)"
+service_broker="$(getConfItem service_broker)"
+service_plan="$(getConfItem service_plan)"
 
-cf enable-service-access "${service_name}" -b "$(getConfItem service_broker)" -p  "$(getConfItem service_plan)" -o test
-cf create-service "${service_name}" "$(getConfItem service_plan)" "${service_name}" -b "$(getConfItem service_broker)" --wait
+cf create-org "test"
+cf target -o "test"
+cf create-space "test_app"
+cf target -s "test_app"
+
+cf enable-service-access "${service_name}" -b "${service_broker}" -p  "${service_plan}" -o test
+cf create-service "${service_name}" "${service_plan}" "${service_name}" -b "${service_broker}" --wait
 
 pushd "${app_dir}" >/dev/null
-cf push\
-  --var app_name=test_app\
-  --var app_domain="$(getConfItem apps_domain)"\
-  --var service_name="${service_name}"\
-  --var instances=1\
-  --var memory_mb="$(getConfItem node_memory_limit||echo 128)"\
-  --var buildpack="binary_buildpack"\
-  -f "manifest.yml"\
+cf push \
+  --var app_name="test_app" \
+  --var app_domain="${app_domain}" \
+  --var service_name="${service_name}" \
+  --var instances=1 \
+  --var memory_mb="${memory_mb}" \
+  --var buildpacks="binary_buildpack" \
+  -f "manifest.yml" \
   -c ./app
 popd > /dev/null
