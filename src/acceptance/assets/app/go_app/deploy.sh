baf4da71bbd74bd1cbc9aa8a9ffc8ba23c66fb7c
Josua Geiger <josua.geiger@sap.com>
2024-02-29 11:27:30 +0100

    fix formatting

--- src/acceptance/assets/app/go_app/deploy.sh
+++ src/acceptance/assets/app/go_app/deploy.sh
@@ -19,57 +19,57 @@ function getConfItem(){
 }
 
 function check_requirements(){
-    if [ -z "${CONFIG_CONTENT}" ]; then
-      echo "ERROR: Couldn't read content of config, please supply the path to config using CONFIG env variable."
-      exit 1
-    fi
+  if [ -z "${CONFIG_CONTENT}" ]; then
+    echo "ERROR: Couldn't read content of config, please supply the path to config using CONFIG env variable."
+    exit 1
+  fi
 }
 
 function deploy(){
   local org space
-    org="test"
-    space="test_$(whoami)"
+  org="test"
+  space="test_$(whoami)"
 
-    cf create-org "${org}"
-    cf target -o "${org}"
-    cf create-space "${space}"
-    cf target -s "${space}"
+  cf create-org "${org}"
+  cf target -o "${org}"
+  cf create-space "${space}"
+  cf target -s "${space}"
 
-    local app_name app_domain service_name memory_mb service_broker service_plan service_offering_enabled
-    app_name="test_app"
-    app_domain="$(getConfItem apps_domain)"
-    service_name="$(getConfItem service_name)"
-    memory_mb="$(getConfItem node_memory_limit || echo 128)"
-    service_broker="$(getConfItem service_broker)"
-    service_plan="$(getConfItem service_plan)"
-    service_offering_enabled="$(getConfItem service_offering_enabled || echo false)"
+  local app_name app_domain service_name memory_mb service_broker service_plan service_offering_enabled
+  app_name="test_app"
+  app_domain="$(getConfItem apps_domain)"
+  service_name="$(getConfItem service_name)"
+  memory_mb="$(getConfItem node_memory_limit || echo 128)"
+  service_broker="$(getConfItem service_broker)"
+  service_plan="$(getConfItem service_plan)"
+  service_offering_enabled="$(getConfItem service_offering_enabled || echo false)"
 
-    # create app upfront to avoid restaging after binding to service happened
-    cf create-app "${app_name}"
+  # create app upfront to avoid restaging after binding to service happened
+  cf create-app "${app_name}"
 
-    if ${service_offering_enabled}; then
-      cf enable-service-access "${service_name}" -b "${service_broker}" -p  "${service_plan}" -o "${org}"
-      cf create-service "${service_name}" "${service_plan}" "${service_name}" -b "${service_broker}" --wait
-      cf bind-service "${app_name}" "${service_name}"
-    else
-      echo "INFO: Skipping service instance setup because service_offering_enabled is '${service_offering_enabled}'"
-    fi
+  if ${service_offering_enabled}; then
+    cf enable-service-access "${service_name}" -b "${service_broker}" -p  "${service_plan}" -o "${org}"
+    cf create-service "${service_name}" "${service_plan}" "${service_name}" -b "${service_broker}" --wait
+    cf bind-service "${app_name}" "${service_name}"
+  else
+    echo "INFO: Skipping service instance setup because service_offering_enabled is '${service_offering_enabled}'"
+  fi
 
-    # make sure that the current directory is the one which contains the build artifacts like binary and manifest.yml
-    local script_dir app_dir
-    script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-    app_dir="$(realpath -e "${script_dir}/build")"
-    pushd "${app_dir}" >/dev/null
-    cf push \
-      --var app_name="${app_name}" \
-      --var app_domain="${app_domain}" \
-      --var service_name="${service_name}" \
-      --var instances=1 \
-      --var memory_mb="${memory_mb}" \
-      -b "binary_buildpack" \
-      -f "manifest.yml" \
-      -c "./app"
-    popd > /dev/null
+  # make sure that the current directory is the one which contains the build artifacts like binary and manifest.yml
+  local script_dir app_dir
+  script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+  app_dir="$(realpath -e "${script_dir}/build")"
+  pushd "${app_dir}" >/dev/null
+  cf push \
+    --var app_name="${app_name}" \
+    --var app_domain="${app_domain}" \
+    --var service_name="${service_name}" \
+    --var instances=1 \
+    --var memory_mb="${memory_mb}" \
+    -b "binary_buildpack" \
+    -f "manifest.yml" \
+    -c "./app"
+  popd > /dev/null
 }
 
 function main(){
