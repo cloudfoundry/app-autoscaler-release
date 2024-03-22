459641b1008a6965e8423c23b5a04bff729c5117
geigerj0 <112163019+geigerj0@users.noreply.github.com>
2024-03-22 12:58:24 +0100

    document acceptance test a bit

--- src/acceptance/app/dynamic_policy_test.go
+++ src/acceptance/app/dynamic_policy_test.go
@@ -262,12 +262,23 @@ var _ = Describe("AutoScaler dynamic policy", func() {
 		})
 
 		It("should scale out and in", func() {
+			// this test depends on the size of the diego cells, currently 8 CPU 32 GB RAM,
+			// and the CPU entitlements per share configured in ci/operations/set-cpu-entitlement-per-share.yaml.
+			// if any of these dependencies change, the test may need some adjustments as well.
+			//
+			// diego cell = 8 CPU 32GB RAM
+			// total shares = 1024 * 32[gb host ram] / 8[upper limit of app memory] = 4096
+			// CPU entitlement per share = 8[number host CPUs] * 100/ 4096[total shares] = 0,1953
+			// CPU entitlement = 4096[total shares] / (32[gb host ram] * 1024) * (1[app memory in GB] * 1024) * 0,1953 ~= 25%
+			//
 			// 1GB app memory results in a maximum cpu entitlement of 25%.
 			// this means that cpuutil will be 100% if app cpu is at 25%.
-			SetAppMemory(cfg, appName, "1GB")
 
-			maximumOfCPUEntitlement := 25
-			AppSetCpuUsage(cfg, appName, maximumOfCPUEntitlement, 5)
+			SetAppMemory(cfg, appName, "1GB")
+			cpuEntitlementOfAppWith1GBMemory := 25
+			maxCPUUsage := cpuEntitlementOfAppWith1GBMemory
+
+			AppSetCpuUsage(cfg, appName, maxCPUUsage, 5)
 			WaitForNInstancesRunning(appGUID, 2, 5*time.Minute)
 
 			//only hit the one instance that was asked to run hot.
