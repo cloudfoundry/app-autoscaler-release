bf54e426fde7c8990bdacd49a63fb168cbdc903b
geigerj0 <112163019+geigerj0@users.noreply.github.com>
2024-04-04 17:50:10 +0200

    also compile ginkgo and provide option to make use of ginkgo binary during /bin/test execution

--- scripts/compile-acceptance-tests.sh
+++ scripts/compile-acceptance-tests.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-compile() {
-  local suites operating_systems architectures
-  suites=("api" "app" "broker" "post_upgrade" "pre_upgrade" "run_performance" "setup_performance")
-  operating_systems=("linux" "darwin")
-  architectures=("amd64" "arm64") # [amd64: intel 64 bit chips]  [arm64: apple silicon chips]
+readonly SUITES=("api" "app" "broker" "post_upgrade" "pre_upgrade" "run_performance" "setup_performance")
+readonly OPERATING_SYSTEMS=("linux" "darwin")
+readonly ARCHITECTURES=("amd64" "arm64") # [amd64: intel 64 bit chips]  [arm64: apple silicon chips]
 
-  for suite in "${suites[@]}"; do
-      for os in "${operating_systems[@]}"; do
-        for arch in "${architectures[@]}"; do
+compile_suites() {
+  for suite in "${SUITES[@]}"; do
+      for os in "${OPERATING_SYSTEMS[@]}"; do
+        for arch in "${ARCHITECTURES[@]}"; do
           GOOS="${os}" GOARCH="${arch}" ginkgo build "src/acceptance/${suite}"
 
           # adjust binary name to include os and architecture information
@@ -19,8 +18,21 @@ compile() {
   done
 }
 
+compile_ginkgo() {
+  pushd ./src/acceptance > /dev/null
+    for os in "${OPERATING_SYSTEMS[@]}"; do
+      for arch in "${ARCHITECTURES[@]}"; do
+        binary_name="ginkgo_v2_${os}_${arch}"
+        GOOS="${os}" GOARCH="${arch}" go build -o "ginkgo_v2_${os}_${arch}" github.com/onsi/ginkgo/v2/ginkgo
+        chmod +x "${binary_name}"
+      done
+    done
+  popd > /dev/null
+}
+
 main() {
-  compile
+  compile_suites
+  compile_ginkgo
 }
 
 main
