15416b8eb6b8ec26ec8bb4aa1486e67d2618ec6b
Alan Moran <bonzofenix@gmail.com>
2024-05-28 12:33:55 +0200

    Improve unpause pipeline script

--- ci/scripts/unpause-pipeline.sh
+++ ci/scripts/unpause-pipeline.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2086
+#
 
-target=$(yq -r ".targets | keys | .[0]" ~/.flyrc)
+target="app-autoscaler-release"
 
 function unpause-job(){
-  pipeline=$1
-  jobs=$(fly -t "$target" jobs -p "$pipeline" --json | jq ".[] | select(.paused==true) | .name" -r )
-  selected_job=$(gum choose --no-limit "$jobs" --header "Select jobs to unpause from pipeline $pipeline"
-)
+  pipeline="$1"
+  jobs=$(fly -t "$target" jobs -p "$pipeline" --json | jq ".[] | select(.paused==true) | .name" -r)
+  selected_job=$(gum choose --no-limit $jobs --header "Select jobs to unpause from pipeline $pipeline")
+
+
+  if [[ -z "$selected_job" ]]; then
+    echo "No job selected to unpause"
+    return
+  fi
 
   for j in $selected_job; do
-    fly -t "$target" unpause-job -j "$pipeline"/"$j"
+    fly -t "$target" unpause-job -j "$pipeline/$j"
   done
 }
 
@@ -17,7 +24,8 @@ function unpause-pipeline(){
   payload=$(fly -t "$target" pipelines --json)
 
   pipelines=$(echo "$payload" | jq ".[] |.name" -r | sort)
-  pipeline=$(gum choose "$pipelines" "all")
+  # ignore shellcheck warning
+  pipeline=$(gum choose $pipelines "all")
 
   if [[ "$pipeline" == "all" ]]; then
     for p in $pipelines; do
@@ -31,5 +39,14 @@ function unpause-pipeline(){
 
 }
 
+function check-login(){
+  if ! fly -t "$target" status;  then
+    echo
+    echo "fly -t $target login"
+    echo
+    exit 1
+  fi
+}
 
+check-login
 unpause-pipeline "${@:-}"
