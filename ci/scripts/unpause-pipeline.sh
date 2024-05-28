dfb43f44bac97c058774d742544d9d379f9f7e96
Alan Moran <bonzofenix@gmail.com>
2024-05-28 11:54:34 +0200

    Fix shellcheck recomendations

--- ci/scripts/unpause-pipeline.sh
+++ ci/scripts/unpause-pipeline.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-target=$( cat ~/.flyrc | yq  ".targets | keys | .[0]")
+target=$(yq -r ".targets | keys | .[0]" ~/.flyrc)
 
 function unpause-job(){
   pipeline=$1
-  jobs=$(fly -t $target jobs -p $pipeline --json | jq ".[] | select(.paused==true) | .name" -r )
-  selected_job=$(gum choose --no-limit $jobs --header "Select jobs to unpause from pipeline $pipeline"
+  jobs=$(fly -t "$target" jobs -p "$pipeline" --json | jq ".[] | select(.paused==true) | .name" -r )
+  selected_job=$(gum choose --no-limit "$jobs" --header "Select jobs to unpause from pipeline $pipeline"
 )
 
   for j in $selected_job; do
-    fly -t $target unpause-job -j $pipeline/$j
+    fly -t "$target" unpause-job -j "$pipeline"/"$j"
   done
 }
 
 function unpause-pipeline(){
-  payload=$(fly -t $target pipelines --json)
+  payload=$(fly -t "$target" pipelines --json)
 
   pipelines=$(echo "$payload" | jq ".[] |.name" -r | sort)
-  pipeline=$(gum choose $pipelines "all")
+  pipeline=$(gum choose "$pipelines" "all")
 
   if [[ "$pipeline" == "all" ]]; then
     for p in $pipelines; do
-      fly -t $target unpause-pipeline -p $p
-      unpause-job $p
+      fly -t "$target" unpause-pipeline -p "$p"
+      unpause-job "$p"
     done
   else
-    fly -t $target unpause-pipeline -p $pipeline
-    unpause-job $pipeline
+    fly -t "$target" unpause-pipeline -p "$pipeline"
+    unpause-job "$pipeline"
   fi
 
 }
