fc67f0e57cbda055b78b4f4cf78a48de1d599360
geigerj0 <112163019+geigerj0@users.noreply.github.com>
2024-06-07 16:49:11 +0200

    remove metricsserver and metricsgateway code, adjust integration tests to work with a brand new logcache mock, get rid of appinstancemetrics database table

--- scripts/generate_test_certs.sh
+++ scripts/generate_test_certs.sh
@@ -16,6 +16,11 @@ ${CERTSTRAP} --depot-path "${depot_path}" init --passphrase '' --common-name aut
 mv -f "${depot_path}"/autoscalerCA.crt "${depot_path}"/autoscaler-ca.crt
 mv -f "${depot_path}"/autoscalerCA.key "${depot_path}"/autoscaler-ca.key
 
+# CA to distribute to dummy loggregator_agent certs
+${CERTSTRAP} --depot-path "${depot_path}" init --passphrase '' --common-name loggregatorCA --years "20"
+mv -f "${depot_path}"/loggregatorCA.crt "${depot_path}"/loggregator-ca.crt
+mv -f "${depot_path}"/loggregatorCA.key "${depot_path}"/loggregator-ca.key
+
 # CA to distribute to dummy syslog emitter certs
 ${CERTSTRAP} --depot-path "${depot_path}" init --passphrase '' --common-name LogCacheSyslogServerCA --years "20"
 mv -f "${depot_path}"/LogCacheSyslogServerCA.crt "${depot_path}"/log-cache-syslog-server-ca.crt
@@ -69,7 +74,6 @@ ${CERTSTRAP} --depot-path "${depot_path}" sign scheduler --CA autoscaler-ca --ye
 openssl pkcs12 -export -in "${depot_path}"/scheduler.crt -inkey "${depot_path}"/scheduler.key -out "${depot_path}"/scheduler.p12 -name scheduler -password pass:123456
 keytool -importcert -alias autoscaler -file "${depot_path}"/autoscaler-ca.crt -keystore "${depot_path}"/autoscaler.truststore -storeType pkcs12 -storepass 123456 -noprompt
 
-
 # # loggregator test server certificate
 ${CERTSTRAP} --depot-path "${depot_path}" request-cert --passphrase '' --domain reverselogproxy
 ${CERTSTRAP} --depot-path "${depot_path}" sign reverselogproxy --CA autoscaler-ca --years "20"
@@ -86,9 +90,17 @@ ${CERTSTRAP} --depot-path "${depot_path}" sign metricserver --CA autoscaler-ca -
 ${CERTSTRAP} --depot-path "${depot_path}" request-cert --passphrase '' --domain metricserver_client
 ${CERTSTRAP} --depot-path "${depot_path}" sign metricserver_client --CA autoscaler-ca --years "20"
 
+# metricsforwarder certificate for loggregator_agent
+${CERTSTRAP} --depot-path "${depot_path}" request-cert --passphrase '' --domain metron
+${CERTSTRAP} --depot-path "${depot_path}" sign metron --CA loggregator-ca --years "20"
+
 # metricsforwarder certificate for log-cache-syslog-server
 ${CERTSTRAP} --depot-path "${depot_path}" request-cert --passphrase '' --domain cf-app
 ${CERTSTRAP} --depot-path "${depot_path}" sign cf-app --CA log-cache-syslog-server-ca --years "20"
+
+# log-cache certificate
+${CERTSTRAP} --depot-path "${depot_path}" request-cert --passphrase '' --domain log-cache --ip 127.0.0.1
+${CERTSTRAP} --depot-path "${depot_path}" sign log-cache --CA autoscaler-ca --years "20"
 
 # mTLS client certificate for local testing
 ## certstrap with multiple OU not working at the moment. Pull request is created in the upstream. Therefore, using openssl at the moment
