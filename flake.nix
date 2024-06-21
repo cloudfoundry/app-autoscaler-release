7a1a8e7b676b71d59c0de838936608ddc4078c2b
Jörg Weisbarth <joerg.weisbarth@sap.com>
2024-06-21 14:49:26 +0200

    Revert "Unify inputs of the two flakes;" (Devbox does not like that.)
    This reverts commit 7607fc6efc66464840fbde92148f0813b8a63163.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs-bosh-cli-v7-3-1.url = github:NixOS/nixpkgs/1179c6c3705509ba25bd35196fca507d2a227bd0;
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-bosh-cli-v7-3-1 }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
@@ -14,6 +15,7 @@
 
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsFor-bosh-cli-v7-3-1 = forAllSystems (system: import nixpkgs-bosh-cli-v7-3-1 { inherit system; });
     in {
       packages = forAllSystems (system:
         let
@@ -34,6 +36,7 @@
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
+          pkgs-bosh-cli-v7-3-1 = nixpkgsFor-bosh-cli-v7-3-1.${system};
         in {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
@@ -48,7 +51,7 @@
               # see bosh-utils change https://github.com/cloudfoundry/bosh-utils/commit/f79167bd43f3afc154065edc95799a464a80605f.
               # this blind bsdtar assumption by bosh-utils breaks creating bosh releases in a Nix shell on macOS.
               # a GitHub issue related to this problem can be found here: https://github.com/cloudfoundry/bosh-utils/issues/86.
-              bosh-cli
+              pkgs-bosh-cli-v7-3-1.bosh-cli
               cloudfoundry-cli
               credhub-cli
               delve # go-debugger
