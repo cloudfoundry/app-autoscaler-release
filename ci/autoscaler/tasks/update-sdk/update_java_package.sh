79166308c3556878c0edb5491e34c03bb594b41c
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2024-07-19 08:44:58 +0200

    create packaging scripts
    Update java version to 21.0.3
    
    remove spec.lock to prevent bosh looking in blobs

--- ci/autoscaler/tasks/update-sdk/update_java_package.sh
+++ ci/autoscaler/tasks/update-sdk/update_java_package.sh
@@ -9,7 +9,6 @@ set -euo pipefail
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${script_dir}/vars.source.sh"
-create_pr=${CREATE_PR:-"false"}
 
 JAVA_VERSION=${1:-"21.0.3"} # default java version
 desired_major_version=${JAVA_VERSION%%.*}
@@ -23,13 +22,39 @@ current_java_version=$(find ./packages -type d -name "openjdk-*" -exec bash -c '
 echo "Current Java Major version is ${current_java_version}"
 
 # Step 1 --> Download java...
-source "${script_dir}/download-java.sh" "${JAVA_VERSION}"
+source "${script_dir}/download_java.sh" "${JAVA_VERSION}"
 
 # Step 2 --> update bosh java package references
 echo "updating bosh java packages..."
-mv ./packages/openjdk-"${current_java_version}" ./packages/openjdk-"${desired_major_version}"
 # shellcheck disable=SC2038
 find . -type f ! -name "*.yml" ! -name "update_java_package.sh" ! -path '*/\.*' -exec grep -l "openjdk-${current_java_version}" {} \;| xargs sed -i "s/openjdk-${current_java_version}/openjdk-${desired_major_version}/g"
+
+mv ./packages/openjdk-"${current_java_version}" ./packages/openjdk-"${desired_major_version}"
+
+echo "  -- creating spec file"
+binary_name="sapmachine-jdk-${JAVA_VERSION}_linux-x64_bin.tar.gz"
+cat > "packages/openjdk-$desired_major_version/spec" <<EOF
+---
+name: openjdk-${desired_major_version}
+dependencies: []
+files:
+- binaries/**/${binary_name}  # from https://github.com/SAP/SapMachine/releases/download/sapmachine-${JAVA_VERSION}/${binary_name}
+EOF
+
+echo "  -- creating packaging script "
+cat > "packages/openjdk-$desired_major_version/packaging" <<EOF
+set -ex
+# compile and runtime bosh env vars
+export JAVA_HOME=/var/vcap/packages/openjdk-$desired_major_version
+export PATH=\$JAVA_HOME/bin:\$PATH
+
+cd \${BOSH_INSTALL_TARGET}
+
+# extract jdk from src/binaries/jdk
+tar zxvf \${BOSH_COMPILE_TARGET}/binaries/jdk/*.tar.gz --strip 1
+EOF
+
+rm "packages/openjdk-$desired_major_version/spec.lock" || true
 
 # creates pr
 echo -n "${JAVA_VERSION}" > "${AUTOSCALER_DIR}/version"
