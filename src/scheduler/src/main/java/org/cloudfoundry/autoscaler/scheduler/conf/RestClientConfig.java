ee2793ca8b3eccfc406f5f9622cc03de95d3812f
Alan Moran <bonzofenix@gmail.com>
2024-07-24 15:15:07 +0200

    Remove unused imports
     - Eliminate unnecessary SSL and HTTP client configuration code from RestClientConfig
     - Clean up imports in ConcurrentRequestTest

--- src/scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/RestClientConfig.java
+++ src/scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/RestClientConfig.java
@@ -1,39 +1,11 @@
 package org.cloudfoundry.autoscaler.scheduler.conf;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 import java.time.Duration;
-import javax.net.ssl.SSLContext;
-
-
-import org.apache.hc.client5.http.auth.AuthScope;
-import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.apache.hc.client5.http.ssl.HttpsSupport;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
-import org.apache.hc.core5.ssl.SSLContexts;
-import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestClientConfig {
--- src/scheduler/src/test/java/org/cloudfoundry/autoscaler/scheduler/misc/ConcurrentRequestTest.java
+++ src/scheduler/src/test/java/org/cloudfoundry/autoscaler/scheduler/misc/ConcurrentRequestTest.java
@@ -25,7 +25,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestOperations;
 
