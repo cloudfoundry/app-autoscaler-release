8de86f3395f525e8b41522fbb0cb773afeb3b3fd
Alan Moran <bonzofenix@gmail.com>
2024-11-06 17:07:55 +0100

    Adds xfcc cf endpoint support to scaling engine

--- src/autoscaler/api/publicapiserver/public_api_server.go
+++ src/autoscaler/api/publicapiserver/public_api_server.go
@@ -102,7 +102,12 @@ func (s *PublicApiServer) CreateCFServer() (ifrit.Runner, error) {
 		return nil, err
 	}
 
+	mainRouter := mux.NewRouter()
 	r := s.autoscalerRouter.GetRouter()
+	mainRouter.PathPrefix("/v2").Handler(r)
+	mainRouter.PathPrefix("/v1").Handler(r)
+	mainRouter.PathPrefix("/health").Handler(r)
+	mainRouter.PathPrefix("/").Handler(s.healthRouter)
 
 	return helpers.NewHTTPServer(s.logger.Session("CfServer"), s.conf.VCAPServer, r)
 }
