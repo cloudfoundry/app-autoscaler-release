2c5379db7d1e69b4b4400cf77bfe5038244ea162
Alan Moran <bonzofenix@gmail.com>
2024-11-06 17:07:55 +0100

    Adds xfcc cf endpoint support to scaling engine

--- src/autoscaler/scalingengine/cmd/scalingengine/scalingengine_test.go
+++ src/autoscaler/scalingengine/cmd/scalingengine/scalingengine_test.go
@@ -15,7 +15,9 @@ import (
 	"github.com/onsi/gomega/gbytes"
 
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -278,3 +280,11 @@ var _ = Describe("Main", func() {
 		})
 	})
 })
+
+func setXFCCCertHeader(req *http.Request, orgGuid, spaceGuid string) {
+	xfccClientCert, err := GenerateClientCert(orgGuid, spaceGuid)
+	block, _ := pem.Decode(xfccClientCert)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(block).ShouldNot(BeNil())
+	req.Header.Add("X-Forwarded-Client-Cert", base64.StdEncoding.EncodeToString(block.Bytes))
+}
--- src/autoscaler/scalingengine/server/server_test.go
+++ src/autoscaler/scalingengine/server/server_test.go
@@ -1,6 +1,7 @@
 package server_test
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -70,6 +71,7 @@ var _ = Describe("Server", func() {
 	})
 
 	JustBeforeEach(func() {
+		fmt.Println("serverUrl: ", serverUrl.String())
 		req, err = http.NewRequest(method, serverUrl.String(), bodyReader)
 		Expect(err).NotTo(HaveOccurred())
 		rsp, err = http.DefaultClient.Do(req)
