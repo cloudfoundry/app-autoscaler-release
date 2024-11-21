7547f8eb03c120668d95a2f6d21dcb95de223d6c
Alan Moran <bonzofenix@gmail.com>
2024-11-21 16:02:11 +0100

    WIP

--- src/autoscaler/api/config/config.go
+++ src/autoscaler/api/config/config.go
@@ -108,6 +108,7 @@ type Config struct {
 	PlanCheck                          *PlanCheckConfig              `yaml:"plan_check"`
 	CatalogPath                        string                        `yaml:"catalog_path"`
 	CatalogSchemaPath                  string                        `yaml:"catalog_schema_path"`
+	CfInstanceCert                     string                        `yaml:"cf_instance_cert"`
 	DashboardRedirectURI               string                        `yaml:"dashboard_redirect_uri"`
 	PolicySchemaPath                   string                        `yaml:"policy_schema_path"`
 	Scheduler                          SchedulerConfig               `yaml:"scheduler"`
