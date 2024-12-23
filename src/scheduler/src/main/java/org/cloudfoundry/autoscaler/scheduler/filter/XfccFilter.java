9c37cb64491bae5c2a20dc2e15136902ce28bcb3
Alan Moran <bonzofenix@gmail.com>
2024-12-23 18:36:01 -0300

    Add .trunk to .gitignore for Devbox configuration.

--- src/scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/filter/XfccFilter.java
+++ src/scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/filter/XfccFilter.java
@@ -1,3 +1,5 @@
+package org.cloudfoundry.autoscaler.scheduler.filter;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
