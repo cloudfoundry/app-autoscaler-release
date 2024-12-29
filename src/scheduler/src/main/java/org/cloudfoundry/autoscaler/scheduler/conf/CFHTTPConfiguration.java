48fd6ce7e4a313190776e67b2539d77b32b30664
Alan Moran <bonzofenix@gmail.com>
2024-12-29 17:09:35 -0300

    WIP: Integration test for api - scheduler cf http server endpoint

--- src/scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/CFHTTPConfiguration.java
+++ src/scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/CFHTTPConfiguration.java
@@ -2,12 +2,17 @@ package org.cloudfoundry.autoscaler.scheduler.conf;
 
 import org.apache.catalina.connector.Connector;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.cloudfoundry.autoscaler.scheduler.filter.XfccFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CFHTTPConfiguration {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> httpConnectorCustomizer() {
@@ -17,5 +22,16 @@ public class CFHTTPConfiguration {
             connector.setSecure(false); // Set to false for HTTP
             factory.addAdditionalTomcatConnectors(connector);
         };
+    }
+
+    @Bean
+    public FilterRegistrationBean<XfccFilter> xfccFilterRegistration(XfccFilter xfccFilter) {
+        FilterRegistrationBean<XfccFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(xfccFilter);
+        registrationBean.addUrlPatterns("/*"); // Apply filter to all incoming requests
+        registrationBean.setOrder(1); // Set filter precedence
+
+        logger.info("Registering XFCC Filter for CF Server");
+        return registrationBean;
     }
 }
