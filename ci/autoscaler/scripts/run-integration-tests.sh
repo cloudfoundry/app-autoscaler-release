be18937653aa8cefc80c906fc75c50075e341aa2
Jörg Weisbarth <joerg.weisbarth@sap.com>
2025-05-06 14:58:44 +0200

    🧹 Further clean-up;

--- ci/autoscaler/scripts/run-integration-tests.sh
+++ ci/autoscaler/scripts/run-integration-tests.sh
@@ -12,13 +12,3 @@ ci_prepare_postgres_db # It is assumed that this test runs in isolation. Consequ
 trap 'devbox services stop postgresql' EXIT
 
 CI='true' make --directory='app-autoscaler-release' integration
-
-
-# pg_ctlcluster "$(pg_lsclusters -j | jq --raw-output '.[0].version')" main start
-
-# psql 'postgres://postgres@127.0.0.1:5432' --command='DROP DATABASE IF EXISTS autoscaler'
-# psql 'postgres://postgres@127.0.0.1:5432' --command='CREATE DATABASE autoscaler'
-
-# pushd app-autoscaler-release
-#		CI='true' make integration
-# popd
--- ci/autoscaler/scripts/run-unit-tests.sh
+++ ci/autoscaler/scripts/run-unit-tests.sh
@@ -4,28 +4,12 @@
 
 set -eu -o pipefail
 
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+source "${script_dir}/common.sh"
 
-# 🚧 To-do: Check here and in the Makefile if we can skip that db-setup. It is strange that we run the db for unit-tests!
-
-export DB_HOST="localhost"
-
-# 🚀 Initialise and start the PostgreSQL DBMS-server
-#
-# devbox makes sure that the environment-variables PGHOST and PGDATA are set appropriately.
-#
-# It is assumed that this test runs in isolation. Consequently the database to run the tests on is
-# not already existing.
-initdb
-# pg_ctl will not work as it is not aware of where to create the socket.
-devbox services up postgresql --background
-createuser tests-pg # --superuser  # As this will only be used in a CI-job, we can allow everything within the image.
-
-# pg_ctlcluster "$(pg_lsclusters -j | jq --raw-output '.[0].version')" main start
-
-# psql 'postgres://postgres@127.0.0.1:5432' --command='DROP DATABASE IF EXISTS autoscaler'
-createdb tests-pg # Needed to be done like this, because 'tests-pg' does not have the required priviledges.
-# psql 'postgres://postgres@127.0.0.1:5432' --command='CREATE DATABASE autoscaler'
+export DB_HOST="localhost" # 🚧 To-do: Can we skip this?
+ci_prepare_postgres_db # It is assumed that this test runs in isolation. Consequently the database
+											 # to run the tests on is not already existing.
+trap 'devbox services stop postgresql' EXIT
 
 CI='true' make --directory='./app-autoscaler-release' test
-
-devbox services stop postgresql
