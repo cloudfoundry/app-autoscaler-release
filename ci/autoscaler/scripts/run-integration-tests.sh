1873623a01184ddec8fea1d53c2c9984a9c1293d
Jörg Weisbarth <joerg.weisbarth@sap.com>
2025-05-06 16:09:46 +0200

    🔧 Environment-fixes for the ci-pipeline;

--- ci/autoscaler/scripts/run-integration-tests.sh
+++ ci/autoscaler/scripts/run-integration-tests.sh
@@ -7,6 +7,8 @@ set -eu -o pipefail
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 source "${script_dir}/common.sh"
 
+export PGHOST="${PGHOST:-/code/.devbox/virtenv/postgresql}"
+export PGDATA="${PGDATA:-/code/.devbox/virtenv/postgresql/data}"
 ci_prepare_postgres_db # It is assumed that this test runs in isolation. Consequently the database
 											 # to run the tests on is not already existing.
 trap 'devbox services stop postgresql' EXIT
