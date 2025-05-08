c6befa3701dda2e23f41aefdee821397790c39bf
Alan Moran <bonzofenix@gmail.com>
2025-05-08 13:02:20 +0200

    Refactor app_manager_test.go by moving "Save and query metrics" tests
     - Relocate "Save and query metrics" context block to a different section within app_manager_test.go
     - Maintain logical test structure and ensure all tests remain unchanged

--- src/autoscaler/eventgenerator/aggregator/app_manager_test.go
+++ src/autoscaler/eventgenerator/aggregator/app_manager_test.go
@@ -67,6 +67,95 @@ var _ = Describe("AppManager", func() {
 			appManager.Stop()
 		})
 
+		Context("Save and query metrics", func() {
+			Context("running with 1 node", func() {
+				BeforeEach(func() {
+					policyDB.RetrievePoliciesStub = func() ([]*models.PolicyJson, error) {
+						return []*models.PolicyJson{{AppId: testAppId, PolicyStr: testPolicyStr}}, nil
+					}
+					testAggregator.MetricCacheSizePerApp = 3
+				})
+				It("should be able to save and query metrics", func() {
+					Eventually(policyDB.RetrievePoliciesCallCount).Should(Equal(1))
+					clock.Increment(1 * testAggregator.PolicyPollerInterval)
+					Eventually(policyDB.RetrievePoliciesCallCount).Should(Equal(2))
+
+					appMetric1 := newAppMetric(testAppId, 100)
+					appMetric2 := newAppMetric(testAppId, 200)
+					appMetric3 := newAppMetric(testAppId, 300)
+					appMetric4 := newAppMetric(testAppId, 400)
+
+					anotherAppMetric1 := newAppMetric("another-app-id", 100)
+					anotherAppMetric2 := newAppMetric("another-app-id", 200)
+
+					Expect(appManager.SaveMetricToCache(appMetric1)).To(BeTrue())
+					Expect(appManager.SaveMetricToCache(appMetric2)).To(BeTrue())
+					Expect(appManager.SaveMetricToCache(appMetric3)).To(BeTrue())
+					Expect(appManager.SaveMetricToCache(appMetric4)).To(BeTrue())
+					Expect(appManager.SaveMetricToCache(anotherAppMetric1)).To(BeFalse())
+					Expect(appManager.SaveMetricToCache(anotherAppMetric2)).To(BeFalse())
+
+					By("cache hit")
+					data, err := appManager.QueryAppMetrics(testAppId, "test-metric-type", 300, 500, db.ASC)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(data).To(Equal([]*models.AppMetric{appMetric3, appMetric4}))
+
+					By("cache miss")
+					appMetricDB.RetrieveAppMetricsReturns([]*models.AppMetric{appMetric1, appMetric2}, nil)
+					data, err = appManager.QueryAppMetrics(testAppId, "test-metric-type", 100, 200, db.ASC)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(appMetricDB.RetrieveAppMetricsCallCount()).To(Equal(1))
+					Expect(data).To(Equal([]*models.AppMetric{appMetric1, appMetric2}))
+
+				})
+			})
+
+			Context("when running with 3 nodes and current node index is 0", func() {
+				BeforeEach(func() {
+					testPool.TotalInstances = 3
+					testPool.InstanceIndex = 0
+					var i int
+					policyDB.RetrievePoliciesStub = func() ([]*models.PolicyJson, error) {
+						i++
+						switch i {
+						case 1:
+							return []*models.PolicyJson{
+								{AppId: "app-id-1", PolicyStr: testPolicyStr},
+								{AppId: "app-id-3", PolicyStr: testPolicyStr},
+							}, nil
+						case 2:
+							return []*models.PolicyJson{
+								{AppId: "app-id-2", PolicyStr: testPolicyStr},
+								{AppId: "app-id-4", PolicyStr: testPolicyStr},
+							}, nil
+						}
+						return []*models.PolicyJson{}, nil
+					}
+
+				})
+				It("caches app shard 0", func() {
+					appMetric1 := newAppMetric("app-id-1", 100)
+					appMetric2 := newAppMetric("app-id-2", 100)
+					appMetric3 := newAppMetric("app-id-3", 100)
+					appMetric4 := newAppMetric("app-id-4", 100)
+
+					Eventually(appManager.GetPolicies).Should(HaveLen(1))
+
+					Expect(appManager.SaveMetricToCache(appMetric1)).To(BeFalse())
+					Expect(appManager.SaveMetricToCache(appMetric2)).To(BeFalse())
+					Expect(appManager.SaveMetricToCache(appMetric3)).To(BeTrue())
+					Expect(appManager.SaveMetricToCache(appMetric4)).To(BeFalse())
+
+					clock.Increment(1 * testAggregator.PolicyPollerInterval)
+					Consistently(appManager.GetPolicies).Should(HaveLen(1))
+					Expect(appManager.SaveMetricToCache(appMetric1)).To(BeFalse())
+					Expect(appManager.SaveMetricToCache(appMetric2)).To(BeFalse())
+					Expect(appManager.SaveMetricToCache(appMetric3)).To(BeFalse())
+					Expect(appManager.SaveMetricToCache(appMetric4)).To(BeTrue())
+				})
+			})
+		})
+
 		Context("when the AppManager is started", func() {
 			BeforeEach(func() {
 				policyDB.RetrievePoliciesStub = func() ([]*models.PolicyJson, error) {
@@ -224,109 +313,6 @@ var _ = Describe("AppManager", func() {
 				})
 			})
 		})
-	})
-
-	Context("Save and query metrics", func() {
-		JustBeforeEach(func() {
-			appManager = NewAppManager(logger, clock, testAggregator, testPool, policyDB, appMetricDB)
-			appManager.Start()
-
-		})
-
-		AfterEach(func() {
-			appManager.Stop()
-		})
-
-		Context("running with 1 node", func() {
-			BeforeEach(func() {
-				policyDB.RetrievePoliciesStub = func() ([]*models.PolicyJson, error) {
-					return []*models.PolicyJson{{AppId: testAppId, PolicyStr: testPolicyStr}}, nil
-				}
-				testAggregator.MetricCacheSizePerApp = 3
-			})
-			It("should be able to save and query metrics", func() {
-				Eventually(policyDB.RetrievePoliciesCallCount).Should(Equal(1))
-				clock.Increment(1 * testAggregator.PolicyPollerInterval)
-				Eventually(policyDB.RetrievePoliciesCallCount).Should(Equal(2))
-
-				appMetric1 := newAppMetric(testAppId, 100)
-
-				appMetric2 := newAppMetric(testAppId, 200)
-
-				appMetric3 := newAppMetric(testAppId, 300)
-
-				appMetric4 := newAppMetric(testAppId, 400)
-
-				anotherAppMetric1 := newAppMetric("another-app-id", 100)
-				anotherAppMetric2 := newAppMetric("another-app-id", 200)
-
-				Expect(appManager.SaveMetricToCache(appMetric1)).To(BeTrue())
-				Expect(appManager.SaveMetricToCache(appMetric2)).To(BeTrue())
-				Expect(appManager.SaveMetricToCache(appMetric3)).To(BeTrue())
-				Expect(appManager.SaveMetricToCache(appMetric4)).To(BeTrue())
-				Expect(appManager.SaveMetricToCache(anotherAppMetric1)).To(BeFalse())
-				Expect(appManager.SaveMetricToCache(anotherAppMetric2)).To(BeFalse())
-
-				By("cache hit")
-				data, err := appManager.QueryAppMetrics(testAppId, "test-metric-type", 300, 500, db.ASC)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(data).To(Equal([]*models.AppMetric{appMetric3, appMetric4}))
-
-				By("cache miss")
-				appMetricDB.RetrieveAppMetricsReturns([]*models.AppMetric{appMetric1, appMetric2}, nil)
-				data, err = appManager.QueryAppMetrics(testAppId, "test-metric-type", 100, 200, db.ASC)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(appMetricDB.RetrieveAppMetricsCallCount()).To(Equal(1))
-				Expect(data).To(Equal([]*models.AppMetric{appMetric1, appMetric2}))
-
-			})
-		})
-
-		Context("when running with 3 nodes and current node index is 0", func() {
-			BeforeEach(func() {
-				testPool.TotalInstances = 3
-				testPool.InstanceIndex = 0
-				var i int
-				policyDB.RetrievePoliciesStub = func() ([]*models.PolicyJson, error) {
-					i++
-					switch i {
-					case 1:
-						return []*models.PolicyJson{
-							{AppId: "app-id-1", PolicyStr: testPolicyStr},
-							{AppId: "app-id-3", PolicyStr: testPolicyStr},
-						}, nil
-					case 2:
-						return []*models.PolicyJson{
-							{AppId: "app-id-2", PolicyStr: testPolicyStr},
-							{AppId: "app-id-4", PolicyStr: testPolicyStr},
-						}, nil
-					}
-					return []*models.PolicyJson{}, nil
-				}
-
-			})
-			It("caches app shard 0", func() {
-				appMetric1 := newAppMetric("app-id-1", 100)
-				appMetric2 := newAppMetric("app-id-2", 100)
-				appMetric3 := newAppMetric("app-id-3", 100)
-				appMetric4 := newAppMetric("app-id-4", 100)
-
-				Eventually(appManager.GetPolicies).Should(HaveLen(1))
-
-				Expect(appManager.SaveMetricToCache(appMetric1)).To(BeFalse())
-				Expect(appManager.SaveMetricToCache(appMetric2)).To(BeFalse())
-				Expect(appManager.SaveMetricToCache(appMetric3)).To(BeTrue())
-				Expect(appManager.SaveMetricToCache(appMetric4)).To(BeFalse())
-
-				clock.Increment(1 * testAggregator.PolicyPollerInterval)
-				Consistently(appManager.GetPolicies).Should(HaveLen(1))
-				Expect(appManager.SaveMetricToCache(appMetric1)).To(BeFalse())
-				Expect(appManager.SaveMetricToCache(appMetric2)).To(BeFalse())
-				Expect(appManager.SaveMetricToCache(appMetric3)).To(BeFalse())
-				Expect(appManager.SaveMetricToCache(appMetric4)).To(BeTrue())
-			})
-		})
-
 	})
 
 	Context("Stop", func() {
