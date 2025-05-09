5ed1245ae841b3d1fc87555c1ead3ca02761f138
Jörg Weisbarth <joerg.weisbarth@sap.com>
2025-05-09 08:04:43 +0200

    🔧 Fix another missing bosh-login;

--- ci/autoscaler/scripts/fetch-latest-stemcell.sh
+++ ci/autoscaler/scripts/fetch-latest-stemcell.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-set -euo pipefail
+set -eu -o pipefail
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "${script_dir}/vars.source.sh"
 source "${script_dir}/common.sh"
 
-
 function main(){
-  bosh_login
-  bosh upload-stemcell --sha1 "sha256:$(cat gcp-jammy-stemcell/sha256)" "$(cat gcp-jammy-stemcell/url)"
+	bosh_login "${BBL_STATE_PATH}"
+	bosh upload-stemcell --sha1 "sha256:$(cat gcp-jammy-stemcell/sha256)" "$(cat gcp-jammy-stemcell/url)"
 }
 
 [ "${BASH_SOURCE[0]}" == "${0}" ] && main "$@"
