d80086944e14a9a86c0295f9814c719baa9d3426
Alan Moran <bonzofenix@gmail.com>
2025-05-12 14:05:14 +0200

    Added sourcing of vars.source.sh and updated bosh_login call with BBL_STATE_PATH in uaa-login.sh

--- ci/autoscaler/scripts/uaa-login.sh
+++ ci/autoscaler/scripts/uaa-login.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${script_dir}/common.sh"
+source "${script_dir}/vars.source.sh"
 
-bosh_login
+bosh_login "${BBL_STATE_PATH}"
 uaa_login
