9254dafc77beb92a0f47c8e7ea0d8abf47c9e70d
Alan Moran <bonzofenix@gmail.com>
2025-05-13 12:30:28 +0200

    Remove Prometheus deployment and related components from CI pipeline
     - Delete Prometheus deployment tasks, scripts, and related ops files.
     - Remove Prometheus BOSH release and alert silencing script.
     - Eliminate references to Prometheus in pipeline and README.
     - Update combine-ops script to exclude Prometheus ops files.

--- ci/infrastructure/scripts/combine-ops.sh
+++ ci/infrastructure/scripts/combine-ops.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 mkdir -p combined-ops/operations
 mkdir -p combined-ops/operations/cf
 mkdir -p combined-ops/operations/autoscaler
-mkdir -p combined-ops/operations/prometheus/operators/cf
 
 cp -r cf-deployment/operations/* combined-ops/operations/cf/
 cp -r ci/ci/operations/* combined-ops/operations/autoscaler
-cp -r prometheus-boshrelease/manifests/operators/cf/* combined-ops/operations/prometheus/operators/cf
