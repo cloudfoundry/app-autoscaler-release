d87e77ce71c90b6555767af41b1a49c7fa581a35
Alan Moran <bonzofenix@gmail.com>
2025-05-29 15:56:04 +0200

    Remove unused tlsCert variable from loadVcapConfig function in config.go

--- src/autoscaler/api/config/config.go
+++ src/autoscaler/api/config/config.go
@@ -172,7 +172,6 @@ func loadPublicApiServerConfig(conf *Config, vcapReader configutil.VCAPConfigura
 }
 
 func loadVcapConfig(conf *Config, vcapReader configutil.VCAPConfigurationReader) error {
-	tlsCert := vcapReader.GetInstanceTLSCerts()
 	if !vcapReader.IsRunningOnCF() {
 		return nil
 	}
