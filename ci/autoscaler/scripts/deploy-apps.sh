297fe5e08b6d9c86acfcace656e02eb0331f5358
Alan Moran <alan.moran@sap.com>
2025-06-24 16:44:12 +0200

    Set dynamic versioning for mta-deploy and specify MTAR_FILENAME in Makefilet pu
     - Introduce dynamic version assignment using PR_NUMBER for mta-deploy in deploy-apps.sh
     - Update Makefile to use MTAR_FILENAME variable during cf deploy command

--- ci/autoscaler/scripts/deploy-apps.sh
+++ ci/autoscaler/scripts/deploy-apps.sh
@@ -11,8 +11,7 @@ bosh_login "${BBL_STATE_PATH}"
 function deploy() {
 	log "Deploying autoscaler apps for bosh deployment '${deployment_name}' "
 	pushd "${autoscaler_dir}/src/autoscaler" > /dev/null
-
-		make mta-deploy
+		VERSION="0.0.0-rc.${PR_NUMBER:-0}" make mta-deploy
 	popd > /dev/null
 }
 
