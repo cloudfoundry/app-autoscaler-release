7dee260711ea25dc2df27b56c082ad3e84ad89da
bonzofenix <317403+bonzofenix@users.noreply.github.com>
2025-10-31 14:31:59 +0100

    Consumes app-autoscaler from submodule (#3975)
    * Consumes app-autoscaler from submodule
    
    * Enable submodules checkout for GitHub Actions workflows
    
    * Update bosh-release-checks workflow to check for changes in 'packages/' directory only
    
    * Update dependency-updates-post-processing workflow to handle submodules
    
     - Ensure submodules are initialized and updated recursively after checking out previous and current branch heads in
     the dependency-updates-post-processing workflow.
    
    * Optimize git add in post-processing workflow to target specific directories
    
    * Improve dependency update workflow by cleaning up generated files and refining change detection scope
    
    * Improve submodule handling in CI workflow
    
     - Add cleanup and deinitialization of submodules to prevent conflicts during branch switches
     - Ensure untracked files in 'src/autoscaler' submodule are cleaned before and after code generation

--- src/autoscaler/scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/dao/PolicyJsonDao.java
+++
@@ -1,9 +0,0 @@
-package org.cloudfoundry.autoscaler.scheduler.dao;
-
-import java.util.List;
-import org.cloudfoundry.autoscaler.scheduler.entity.PolicyJsonEntity;
-
-public interface PolicyJsonDao {
-
-  public List<PolicyJsonEntity> getAllPolicies();
-}
