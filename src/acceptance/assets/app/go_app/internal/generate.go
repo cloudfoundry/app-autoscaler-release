90eac25874c3fdcf13f17286771446e4a2b11885
Alan Moran <bonzofenix@gmail.com>
2025-11-20 13:15:52 +0100

    Remove src/autoscaler

--- src/acceptance/assets/app/go_app/internal/generate.go
+++
@@ -1,5 +0,0 @@
-package internal
-
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --config ogen-config.yaml --target applicationmetric --clean ../../../../../../api/application-metric-api.openapi.yaml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --config ogen-config.yaml --target custommetrics --clean ../../../../../../api/custom-metrics-api.openapi.yaml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --config ogen-config.yaml --target policy --clean ../../../../../../api/policy-api.openapi.yaml
